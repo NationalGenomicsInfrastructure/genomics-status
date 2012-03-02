574fddf62fa2455f697187b445b550acef71fad9
Valentine Svensson <valentine@nxn.se>
2012-03-02 09:14:21 +0100

    Initial commit

---
+++ static/size_bars.js
@@ -0,0 +1,102 @@
+// String helper thing
+String.prototype.format = function() {
+  var args = arguments;
+   return this.replace(/{(\d+)}/g, function(match, number) { 
+    return typeof args[number] != 'undefined' ? args[number]
+      : match
+    ;
+  });
+};
+
+// Some example data
+
+var data = [];
+for (counter=0; counter < 24; counter++) {
+   data.push({
+  "date": "2011-12-{0}".format(counter),
+  "machine": "SN167",
+  "project": "0302_AD07L0ACXX",
+  "size": "{0}000000".format(counter % 5)
+  });
+}
+
+
+// var data = [log1, log2, log3, log4];
+
+// Construct linear scales
+
+var w = 20,
+   h = 300;
+
+var x = d3.scale.linear()
+   .domain([0, 1])
+   .range([0, w]);
+
+var y = d3.scale.linear()
+   .domain([0, 5000000])
+   .rangeRound([0, h]);
+
+// Construct SVG container
+
+var chart = d3.select("article").append("svg")
+    .attr("class", "chart")
+    .attr("width", w * data.length - 1)
+    .attr("height", h);
+
+// Add a line to indicate where the graph is
+
+ chart.append("line")
+     .attr("x1", 0)
+     .attr("x2", w * data.length)
+     .attr("y1", h - 0.5)
+     .attr("y2", h - 0.5)
+     .style("stroke", "#000");
+
+// Draw graph
+// init_draw();
+
+// Drawing functions
+
+function init_draw() {
+
+    chart.selectAll("rect")
+        .data(data)
+      .enter().insert("rect", "line")
+        .attr("x", function(d, i) { return x(i) - 0.5; })
+        .attr("y", h)
+        .attr("width", w);
+
+   // Update
+   chart.selectAll("rect")
+       .data(data)
+     .transition()
+       .duration(400)
+       .attr("y", function(d) { return h - y(d.size) - 0.5; })
+       .attr("height", function(d) { return y(d.size); });
+ 
+ }
+
+function redraw() {
+
+    var rect = chart.selectAll("rect")
+        .data(data, function(d) { return d.date; });
+  
+    rect.enter().insert("rect", "line")
+        .attr("x", function(d, i) { return x(i + 1) - 0.5; })
+        .attr("y", function(d) { return h - y(d.size) - 0.5; })
+        .attr("width", w)
+       .attr("height", function(d) { return y(d.size); })
+     .transition()
+       .duration(300)
+       .attr("x", function(d, i) { return x(i) - 0.5; });
+ 
+    rect.transition()
+        .duration(300)
+        .attr("x", function(d, i) { return x(i) - 0.5; });
+  
+    rect.exit().transition()
+        .duration(300)
+        .attr("x", function(d, i) { return x(i - 1) - 0.5; })
+        .remove();
+ 
+ }
---
+++ tornado_log_app.py
@@ -0,0 +1,232 @@
+import time
+import os
+
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+
+import pika
+from pika.adapters.tornado_connection import TornadoConnection
+
+PORT = 8888
+
+
+class ExampleDataIterator(object):
+    def __init__(self):
+        self.i = 24
+
+    def next(self):
+        self.i += 1
+        return {"date": "2011-12-%i" % (self.i,),
+                "machine": "SN167",
+                "project": "0302_AD07L0ACXX",
+                "size": "%i000000" % (self.i % 4 + 1,)}
+
+
+class MessageTransport(object):
+    senders = set()
+    message = None
+
+    def register_sender(self, callback):
+        cls = MessageTransport
+        cls.senders.add(callback)
+
+    def unregister_sender(self, callback):
+        cls = MessageTransport
+        cls.senders.remove(callback)
+
+    def new_message(self, message):
+        cls = MessageTransport
+        cls.message = message
+
+    def send_messages(self):
+        cls = MessageTransport
+        for send in cls.senders:
+            send(cls.message)
+
+
+class WebSocketMessenger(tornado.websocket.WebSocketHandler, MessageTransport):
+    def open(self):
+        self.data = ExampleDataIterator()
+        self.register_sender(self.write_message)
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        print("Requested more log data")
+        # self.write_message(json.dumps(self.data.next()))
+        self.send_messages()
+
+    def on_close(self):
+        self.unregister_sender(self.write_message)
+        print("WebSocket closed")
+
+
+class PikaClient(MessageTransport):
+
+    def __init__(self):
+        self.queue_name = 'tornado-test-app'
+
+        # Default values
+        self.connected = False
+        self.connecting = False
+        self.connection = None
+        self.channel = None
+
+        # A place for us to keep messages sent to us by Rabbitmq
+        self.messages = list()
+
+        # A place for us to put pending messages while we're waiting to connect
+        self.pending = list()
+
+    def connect(self):
+        if self.connecting:
+            pika.log.info('PikaClient: Already connecting to RabbitMQ')
+            return
+        pika.log.info('PikaClient: Connecting to RabbitMQ on 0.0.0.0:5672')
+        self.connecting = True
+
+        credentials = pika.PlainCredentials('guest', 'guest')
+        param = pika.ConnectionParameters(host='0.0.0.0',
+                                          port=5672,
+                                          virtual_host="/",
+                                          credentials=credentials)
+        self.connection = TornadoConnection(param,
+                                            on_open_callback=self.on_connected)
+        self.connection.add_on_close_callback(self.on_closed)
+
+    def on_connected(self, connection):
+        pika.log.info('PikaClient: Connected to RabbitMQ on 0.0.0.0:5672')
+        self.connected = True
+        self.connection = connection
+        self.connection.channel(self.on_channel_open)
+
+    def on_channel_open(self, channel):
+        pika.log.info('PikaClient: Channel Open, Declaring Exchange')
+        self.channel = channel
+        self.channel.exchange_declare(exchange='tornado',
+                                      type="direct",
+                                      durable=True,
+                                      callback=self.on_exchange_declared)
+
+    def on_exchange_declared(self, frame):
+        pika.log.info('PikaClient: Exchange Declared, Declaring Queue')
+        self.channel.queue_declare(queue=self.queue_name,
+                                   durable=True,
+                                   exclusive=False,
+                                   callback=self.on_queue_declared)
+
+    def on_queue_declared(self, frame):
+        pika.log.info('PikaClient: Queue Declared, Binding Queue')
+        self.channel.queue_bind(exchange='tornado',
+                                queue=self.queue_name,
+                                routing_key='tornado.*',
+                                callback=self.on_queue_bound)
+
+    def on_queue_bound(self, frame):
+        pika.log.info('PikaClient: Queue Bound, Issuing Basic Consume')
+        self.channel.basic_consume(consumer_callback=self.on_pika_message,
+                                   queue=self.queue_name,
+                                   no_ack=True)
+        # Send any messages pending
+        for properties, body in self.pending:
+            self.channel.basic_publish(exchange='tornado',
+                                       routing_key='tornado.*',
+                                       body=body,
+                                       properties=properties)
+
+    def on_pika_message(self, channel, method, header, body):
+        pika.log.info('PikaClient: Message receive, delivery tag #%i' % \
+                     method.delivery_tag)
+        # Append it to our messages list
+        self.messages.append(body)
+        self.new_message(body)
+        self.send_messages()
+
+    def on_basic_cancel(self, frame):
+        pika.log.info('PikaClient: Basic Cancel Ok')
+        # If we don't have any more consumer processes running close
+        self.connection.close()
+
+    def on_closed(self, connection):
+        # We've closed our pika connection so stop the demo
+        tornado.ioloop.IOLoop.instance().stop()
+
+    def sample_message(self, tornado_request):
+        # Build a message to publish to RabbitMQ
+
+        body = "This is a test message."
+
+        # Send the message
+        properties = pika.BasicProperties(content_type="text/plain",
+                                          delivery_mode=1)
+        self.channel.basic_publish(exchange='tornado',
+                                   routing_key='tornado.*',
+                                   body=body,
+                                   properties=properties)
+
+    def get_messages(self):
+        # Get the messages to return, then empty the list
+        output = self.messages
+        self.messages = list()
+        return output
+
+
+class MainHandler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    def get(self):
+        # Send a sample message
+        # self.application.pika.sample_message(self.request)
+
+        # Send our main document
+        self.render("status.html",
+                    connected=self.application.pika.connected)
+
+
+class AjaxHandler(tornado.web.RequestHandler):
+
+    @tornado.web.asynchronous
+    def get(self):
+        # Send a sample message
+        # self.application.pika.sample_message(self.request)
+
+        # Send our output
+        self.set_header("Content-type", "application/json")
+        # self.write(json.dumps(self.application.pika.get_messages()))
+        self.finish()
+
+
+if __name__ == '__main__':
+
+    # Setup the Tornado Application
+    settings = {
+        "debug": True,
+        "static_path": os.path.join(os.path.dirname(__file__), "static")
+    }
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/ajax", AjaxHandler),
+        (r"/websocket", WebSocketMessenger)
+    ], **settings)
+
+    # Helper class PikaClient makes coding async Pika apps in tornado easy
+    pc = PikaClient()
+    application.pika = pc  # We want a shortcut for below for easier typing
+
+    # Set our pika.log options
+    pika.log.setup(color=True)
+
+    # Start the HTTP Server
+    pika.log.info("Starting Tornado HTTPServer on port %i" % PORT)
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(PORT)
+
+    # Get a handle to the instance of IOLoop
+    ioloop = tornado.ioloop.IOLoop.instance()
+
+    # Add our Pika connect to the IOLoop with a deadline in 0.1 seconds
+    ioloop.add_timeout(time.time() + .1, application.pika.connect)
+
+    # Start the IOLoop
+    ioloop.start()
