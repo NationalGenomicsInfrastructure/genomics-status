da766a707582b2e5120cbf39b130aa76ae636fe9
Valentine Svensson <valentine.svensson@scilifelab.se>
2012-04-17 13:20:42 +0200

    Removing RabbitMQ functions

--- status_app.py
+++ status_app.py
@@ -8,9 +8,6 @@ import tornado.websocket
 import tornado.ioloop
 import tornado.web
 
-import pika
-from pika.adapters.tornado_connection import TornadoConnection
-
 from pymongo import Connection
 
 PORT = 8888
@@ -145,117 +142,6 @@ class WebSocketMessenger(tornado.websocket.WebSocketHandler, MessageMixin):
         return storage_load_over_time
 
 
-class PikaClient(MessageMixin):
-
-    def __init__(self):
-        self.queue_name = 'tornado-test-app'
-
-        # Default values
-        self.connected = False
-        self.connecting = False
-        self.connection = None
-        self.channel = None
-
-        # A place for us to keep messages sent to us by Rabbitmq
-        self.messages = list()
-
-        # A place for us to put pending messages
-        # while we're waiting to connect
-        self.pending = list()
-
-    def connect(self):
-        if self.connecting:
-            pika.log.info('PikaClient: Already connecting to RabbitMQ')
-            return
-        pika.log.info('PikaClient: Connecting to RabbitMQ on 0.0.0.0:5672')
-        self.connecting = True
-
-        credentials = pika.PlainCredentials('guest', 'guest')
-        param = pika.ConnectionParameters(host='0.0.0.0',
-                                          port=5672,
-                                          virtual_host="/",
-                                          credentials=credentials)
-        self.connection = TornadoConnection(param,
-                                        on_open_callback=self.on_connected)
-        self.connection.add_on_close_callback(self.on_closed)
-
-    def on_connected(self, connection):
-        pika.log.info('PikaClient: Connected to RabbitMQ on 0.0.0.0:5672')
-        self.connected = True
-        self.connection = connection
-        self.connection.channel(self.on_channel_open)
-
-    def on_channel_open(self, channel):
-        pika.log.info('PikaClient: Channel Open, Declaring Exchange')
-        self.channel = channel
-        self.channel.exchange_declare(exchange='tornado',
-                                      type="direct",
-                                      durable=True,
-                                      callback=self.on_exchange_declared)
-
-    def on_exchange_declared(self, frame):
-        pika.log.info('PikaClient: Exchange Declared, Declaring Queue')
-        self.channel.queue_declare(queue=self.queue_name,
-                                   durable=True,
-                                   exclusive=False,
-                                   callback=self.on_queue_declared)
-
-    def on_queue_declared(self, frame):
-        pika.log.info('PikaClient: Queue Declared, Binding Queue')
-        self.channel.queue_bind(exchange='tornado',
-                                queue=self.queue_name,
-                                routing_key='tornado.*',
-                                callback=self.on_queue_bound)
-
-    def on_queue_bound(self, frame):
-        pika.log.info('PikaClient: Queue Bound, Issuing Basic Consume')
-        self.channel.basic_consume(consumer_callback=self.on_pika_message,
-                                   queue=self.queue_name,
-                                   no_ack=True)
-        # Send any messages pending
-        for properties, body in self.pending:
-            self.channel.basic_publish(exchange='tornado',
-                                       routing_key='tornado.*',
-                                       body=body,
-                                       properties=properties)
-
-    def on_pika_message(self, channel, method, header, body):
-        pika.log.info('PikaClient: Message receive, delivery tag #%i' % \
-                     method.delivery_tag)
-        # Append it to our messages list
-        self.messages.append(body)
-        self.new_message(body)
-        self.send_messages()
-
-    def on_basic_cancel(self, frame):
-        pika.log.info('PikaClient: Basic Cancel Ok')
-        # If we don't have any more consumer processes running close
-        self.connection.close()
-
-    def on_closed(self, connection):
-        # We've closed our pika connection so stop the demo
-        tornado.ioloop.IOLoop.instance().stop()
-
-    def sample_message(self, tornado_request):
-        # Build a message to publish to RabbitMQ
-
-        body = "This is a test message."
-
-        # Send the message
-        properties = pika.BasicProperties(content_type="text/plain",
-                                          delivery_mode=1)
-        self.channel.basic_publish(exchange='tornado',
-                                   routing_key='tornado.*',
-                                   body=body,
-                                   properties=properties)
-
-    def get_messages(self):
-        # Get the messages to return, then empty the list
-        output = self.messages
-        self.messages = list()
-        return output
-
-
 class MainHandler(tornado.web.RequestHandler):
 
     @tornado.web.asynchronous
@@ -270,7 +156,6 @@ class AjaxHandler(tornado.web.RequestHandler):
     def get(self):
         # Send our output
         self.set_header("Content-type", "application/json")
-        # self.write(json.dumps(self.application.pika.get_messages()))
         self.finish()
 
 
@@ -279,7 +164,6 @@ class Application(tornado.web.Application):
         handlers = [
             (r"/", MainHandler),
             (r"/ajax", AjaxHandler),
-            (r"/websocket", WebSocketMessenger)
         ]
         # Setup the Tornado Application
         settings = {
@@ -289,11 +173,6 @@ class Application(tornado.web.Application):
 
         tornado.web.Application.__init__(self, handlers, **settings)
 
-        # Helper class PikaClient makes
-        # coding async Pika apps in tornado easy
-        # pc = PikaClient()
-        # self.pika = pc  # We want a shortcut for below for easier typing
-
         # Global connection to the log database
         connection = Connection()
         self.db = connection.logs
@@ -301,14 +180,10 @@ class Application(tornado.web.Application):
 
 
 def main():
-    # Set pika.log options
-    # pika.log.setup(color=True)
-
     # Instantiate Application
     application = Application()
 
     # Start HTTP Server
-    pika.log.info("Starting Tornado HTTPServer on port %i" % PORT)
     http_server = tornado.httpserver.HTTPServer(application)
     http_server.listen(PORT)
 
