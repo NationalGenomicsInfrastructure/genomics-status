c6cd3f9c773d6518bea74442c4f0112ec9d814f6
Valentine Svensson <valentine.svensson@scilifelab.se>
2012-11-13 19:52:54 +0100

    Made a plot page for % bases >= Q30

--- status_app.py
+++ status_app.py
@@ -431,9 +431,7 @@ class ReadsVsQDataHandler(tornado.web.RequestHandler):
 
 class BarcodeVsExpectedDataHandler(tornado.web.RequestHandler):
     def get(self):
-        start = self.get_argument("start", None)
         self.set_header("Content-type", "application/json")
-        # self.write(json.dumps(start, default=dthandler))
         self.write(json.dumps(self.yield_difference(), default=dthandler))
 
     def yield_difference(self):
@@ -588,6 +586,71 @@ class FlowcellDemultiplexHandler(tornado.web.RequestHandler):
             lane_qc[row.key[1]] = row.value
 
         return lane_qc
+
+import cStringIO
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+import matplotlib.pyplot as plt
+
+
+class Q30PlotHandler(tornado.web.RequestHandler):
+    def get(self):
+
+        view = self.application.flowcells_db.view("lanes/gtq30", group_level=2)
+
+        flowcells = OrderedDict()
+        instruments = set()
+
+        for row in view:
+            flowcells[tuple(row.key)] = {"q30": row.value["sum"] / row.value["count"],
+                                         "instrument": row.value["instrument"]}
+            instruments.add(row.value["instrument"])
+
+        i_to_n = dict(zip(instruments, np.linspace(0., 1., len(instruments))))
+
+        cmap = plt.cm.Dark2
+
+        fig = Figure(figsize=[12, 6])
+        ax = fig.add_axes([0.1, 0.2, 0.8, 0.7])
+
+        locs, labels = [], []
+        for instrument in instruments:
+            color = cmap(i_to_n[instrument])
+            X = [(k, i["q30"]) for k, i in flowcells.items() if "q30" in i and i["instrument"] == instrument]
+            y = [x[1] for x in X]
+            x = [parser.parse(x[0][0].split("_")[0]) for x in X]
+
+            ax.scatter(x, y, c=color, s=150, marker='o', label=instrument)
+
+            locs += x
+            labels += [a[0][0].split("_")[0] for a in X]
+
+        L = list(set(zip(locs, labels)))
+
+        ax.set_xticks([l[0] for l in L])
+        ax.set_xticklabels([l[1] for l in L], rotation=90)
+        ax.set_xlabel("Run")
+        ax.set_ylabel("%")
+        ax.set_ylim([0, 100])
+
+        ax.legend(loc="lower right", bbox_to_anchor=(1, 1), ncol=5)
+
+        FigureCanvasAgg(fig)
+
+        buf = cStringIO.StringIO()
+        fig.savefig(buf, format="png")
+        data = buf.getvalue()
+
+        self.set_header("Content-Type", "image/png")
+        self.set_header("Content-Length", len(data))
+        self.write(data)
+
+
+class Q30Handler(tornado.web.RequestHandler):
+    def get(self):
+        t = self.application.loader.load("q30.html")
+        self.write(t.generate())
 
 
 class FlowcellQ30Handler(tornado.web.RequestHandler):
@@ -776,6 +839,7 @@ class Application(tornado.web.Application):
                 FlowcellDemultiplexHandler),
             ("/api/v1/flowcell_q30/([^/]*)$", FlowcellQ30Handler),
             ("/api/v1/flowcells/([^/]*)$", FlowcellDataHandler),
+            ("/api/v1/plot/q30.png", Q30PlotHandler),
             ("/api/v1/picea_home", PiceaHomeDataHandler),
             ("/api/v1/picea_home/users/", PiceaUsersDataHandler),
             ("/api/v1/picea_home/([^/]*)$", PiceaHomeUserDataHandler),
@@ -811,6 +875,7 @@ class Application(tornado.web.Application):
             # ("/expected", ExpectedHandler),
             ("/flowcells", FlowcellsHandler),
             ("/flowcells/([^/]*)$", FlowcellHandler),
+            ("/q30", Q30Handler),
             ("/picea", PiceaHandler),
             ("/qc", QCHandler),
             ("/qc/([^/]*)$", SampleQCSummaryHandler),
@@ -859,6 +924,7 @@ class Application(tornado.web.Application):
         tornado.autoreload.watch("design/applications.html")
         tornado.autoreload.watch("design/barcodes.html")
         tornado.autoreload.watch("design/amanita.html")
+        tornado.autoreload.watch("design/q30.html")
         tornado.autoreload.watch("design/expected.html")
         tornado.autoreload.watch("design/flowcells.html")
         tornado.autoreload.watch("design/application.html")
