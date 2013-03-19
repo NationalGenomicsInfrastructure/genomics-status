8f4cc128ca7972a32a9f2c40982b54d986ac328f
Valentine Svensson <valentine.svensson@scilifelab.se>
2013-03-19 14:28:16 +0100

    Includign setup.py, renamed a submodule (in the python sense)

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""Setup file and install script SciLife python scripts.
+"""
+from setuptools import setup, find_packages
+
+setup(name = "status",
+      author = "Valentine Svensson",
+      author_email = "genomics_support@scilifelab.se",
+      description = "Webapp for keeping track of metadata status at SciLifeLab",
+      license = "MIT",
+      scripts = ["status_app.py"],
+      install_requires = [
+        "tornado",
+        "couchdb",
+        ]
+      )
---
+++ status/testing.py
@@ -0,0 +1,30 @@
+""" Status Handlers used to test some functionalities while building layouts.
+"""
+import random
+
+import tornado.web
+
+
+class TestDataHandler(tornado.web.RequestHandler):
+    def get(self, n):
+        self.set_header("Content-type", "application/json")
+        self.write(json.dumps(self.random_series(int(n)), default=dthandler))
+
+    def random_series(self, n):
+        s = [{"y":random.randint(10, 99), "x":i} for i in xrange(int(n))]
+        d = dict()
+        d["data"] = s
+        d["name"] = "series"
+        return [d]
+
+
+class TestGridHandler(tornado.web.RequestHandler):
+    def get(self):
+        t = self.application.loader.load("test_grid.html")
+        self.write(t.generate())
+
+
+class TestHandler(tornado.web.RequestHandler):
+    def get(self):
+        t = self.application.loader.load("test_grid.html")
+        self.write(t.generate())
