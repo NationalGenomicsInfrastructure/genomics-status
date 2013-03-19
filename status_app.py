4a4726c0c5048699491d6678eb9b2680d0cd3594
Valentine Svensson <valentine.svensson@scilifelab.se>
2013-03-19 14:19:45 +0100

    Restructured to get the ability to seperate out handlers, and keping static/layout files for themselves

--- status_app.py
+++ status_app.py
@@ -19,6 +19,8 @@ from collections import defaultdict
 
 import numpy as np
 
+from status.testing import TestDataHandler
+
 
 def dthandler(obj):
     """ISO formatting for datetime to be used in JSON.
@@ -34,21 +36,9 @@ class MainHandler(tornado.web.RequestHandler):
         self.write(t.generate())
 
 
-class TestHandler(tornado.web.RequestHandler):
-    def get(self):
-        t = self.application.loader.load("test_grid.html")
-        self.write(t.generate())
-
-
 class QuotasHandler(tornado.web.RequestHandler):
     def get(self):
         t = self.application.loader.load("quota_grid.html")
-        self.write(t.generate())
-
-
-class TestGridHandler(tornado.web.RequestHandler):
-    def get(self):
-        t = self.application.loader.load("test_grid.html")
         self.write(t.generate())
 
 
@@ -107,19 +97,6 @@ class UppmaxProjectsDataHandler(tornado.web.RequestHandler):
             project_list.append(row.key)
 
         return project_list
-
-
-class TestDataHandler(tornado.web.RequestHandler):
-    def get(self, n):
-        self.set_header("Content-type", "application/json")
-        self.write(json.dumps(self.random_series(int(n)), default=dthandler))
-
-    def random_series(self, n):
-        s = [{"y":random.randint(10, 99), "x":i} for i in xrange(int(n))]
-        d = dict()
-        d["data"] = s
-        d["name"] = "series"
-        return [d]
 
 
 class ProductionDataHandler(tornado.web.RequestHandler):
