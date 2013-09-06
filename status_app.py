b6a075fcf04d672118a73e4db1f411d2284cdaef
Guillermo Carrasco <guille.ch.88@gmail.com>
2013-09-06 16:11:16 +0200

    Fix label overlapp in pie charts. Align charts. Make obvious that rows are clickable

--- status_app.py
+++ status_app.py
@@ -254,21 +254,41 @@ class ApplicationsPlotHandler(ApplicationsDataHandler):
         cmap = plt.cm.prism
         colors = cmap(np.linspace(0., 1., len(applications)))
 
-        pie_wedge_collection = ax.pie(applications.values(),
-                                      colors=colors,
-                                      labels=applications.keys(),
-                                      labeldistance=1.05)
+        #Sort the slices by size (so we know where the thin slices will end up)
+        applications = sorted(applications.items(), key=lambda x: x[1])
 
-        for pie_wedge in pie_wedge_collection[0]:
+        #Reorder the slices so not all small slices end up together (causing
+        #label overlapping)
+        reordered = []
+        n_app = len(applications)
+        for i in range(n_app/2):
+            reordered.append(applications[i])
+            reordered.append(applications[n_app - 1 - i])
+
+        applications_labels = [a[0] for a in reordered]
+        applications_values = [a[1] for a in reordered]
+
+        #All small labels will be concentrated in the bottom right quarter, in
+        #order to put these labels horitzontally (and thus avoid overlapping),
+        #we can start the pie rotated 45.0 degrees
+        pie_wedge_collection = ax.pie(applications_values,
+                                      colors=colors,
+                                      labels=applications_labels,
+                                      labeldistance=1.05,
+                                      startangle=45.0)
+
+        #Set some style parameters
+        for pie_wedge, pie_texts in zip(pie_wedge_collection[0], pie_wedge_collection[1]):
             pie_wedge.set_edgecolor('white')
-            pie_wedge.set_linewidth(2)
+            pie_wedge.set_linewidth(1)
+            pie_texts.set_fontsize(8)
 
         fig.subplots_adjust(left=0.15, right=0.75, bottom=0.15)
 
         FigureCanvasAgg(fig)
 
         buf = cStringIO.StringIO()
-        fig.savefig(buf, format="png")
+        fig.savefig(buf, format="png", bbox_inches='tight', pad_inches=0)
         applications = buf.getvalue()
 
         self.set_header("Content-Type", "image/png")
@@ -303,21 +323,41 @@ class SamplesApplicationsPlotHandler(SamplesApplicationsDataHandler):
         cmap = plt.cm.prism
         colors = cmap(np.linspace(0., 1., len(applications)))
 
-        pie_wedge_collection = ax.pie(applications.values(),
-                                      colors=colors,
-                                      labels=applications.keys(),
-                                      labeldistance=1.05)
+        #Sort the slices by size (so we know where the thin slices will end up)
+        applications = sorted(applications.items(), key=lambda x: x[1])
 
-        for pie_wedge in pie_wedge_collection[0]:
+        #Reorder the slices so not all small slices end up together (causing
+        #label overlapping)
+        reordered = []
+        n_app = len(applications)
+        for i in range(n_app/2):
+            reordered.append(applications[i])
+            reordered.append(applications[n_app - 1 - i])
+
+        applications_labels = [a[0] for a in reordered]
+        applications_values = [a[1] for a in reordered]
+
+        #All small labels will be concentrated in the bottom right quarter, in
+        #order to put these labels horitzontally (and thus avoid overlapping),
+        #we can start the pie rotated 45.0 degrees
+        pie_wedge_collection = ax.pie(applications_values,
+                                      colors=colors,
+                                      labels=applications_labels,
+                                      labeldistance=1.05,
+                                      startangle=0)
+
+        #Set some style parameters
+        for pie_wedge, pie_texts in zip(pie_wedge_collection[0], pie_wedge_collection[1]):
             pie_wedge.set_edgecolor('white')
-            pie_wedge.set_linewidth(2)
+            pie_wedge.set_linewidth(1)
+            pie_texts.set_fontsize(6)
 
         fig.subplots_adjust(left=0.15, right=0.75, bottom=0.15)
 
         FigureCanvasAgg(fig)
 
         buf = cStringIO.StringIO()
-        fig.savefig(buf, format="png")
+        fig.savefig(buf, format="png", bbox_inches='tight', pad_inches=0)
         applications = buf.getvalue()
 
         self.set_header("Content-Type", "image/png")
