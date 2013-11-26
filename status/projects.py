8b5a81e17401880fc790bb1cf75511373cb7bf09
Johannes Alneberg <johannes.alneberg@scilifelab.se>
2013-11-26 11:37:58 +0100

    Projects list can include any project udf.

--- status/projects.py
+++ status/projects.py
@@ -2,6 +2,7 @@
 """
 from collections import OrderedDict
 import json
+import string
 
 import tornado.web
 
@@ -15,8 +16,7 @@ DEFAULT_COLUMNS = OrderedDict([('Project', 'project'),
                                ('Number of Samples','no_samples'),
                                ('Type','type')])
 
-
-OTHER_COLUMNS = OrderedDict([('Queue Date', 'queued'),
+EXTRA_COLUMNS = OrderedDict([('Queue Date', 'queued'),
                              ('Ordered million reads per sample', 'ordered_reads'),
                              ('Source','source'),
                              ('Reference Genome', 'reference_genome'),
@@ -24,8 +24,18 @@ OTHER_COLUMNS = OrderedDict([('Queue Date', 'queued'),
                              ('Customer Reference', 'customer_reference'),
                              ('Sequencing Platform', 'sequencing_platform'),
                              ('Uppnex ID', 'uppnex_id'),
-                             ('Portal ID', 'Portal_id')])
+                             ('Open Date', 'open_date'),
+                             ('Disposal of Samples', 'disposal_of_any_remaining_samples'),
+                             ('All Samples Sequenced', 'all_samples_sequenced'),
+                             ('Portal ID (somewhere else)', 'Portal_id'),
+                             ('Portal ID (Details)', 'portal_id'),
+                             ('Lanes', 'sequence_units_ordered_(lanes)'),
+                             ('Best Practice Bioinformatics', 'best_practice_bioinformatics'),
+                             ('Bioinformatic QC', 'bioinformatic_qc'),
+                             ('Comment', 'comment'),
+                             ('Aborted', 'aborted')])
 
+COLUMNS = dict([('DEFAULT_COLUMNS', DEFAULT_COLUMNS),('EXTRA_COLUMNS', EXTRA_COLUMNS)])
 
 class ProjectsDataHandler(tornado.web.RequestHandler):
     """ Serves brief information for each project in the database.
@@ -39,10 +49,47 @@ class ProjectsDataHandler(tornado.web.RequestHandler):
     def list_projects(self):
         projects = OrderedDict()
         for row in self.application.projects_db.view("project/summary", descending=True):
+            # the details key gives values containing multiple udfs on project level
+            # and project_summary key gives 'temporary' udfs that will move to 'details'.
+            # Include these as normal key:value pairs
+            if 'project_summary' in row.value:
+                for summary_key, summary_value in row.value['project_summary'].iteritems():
+                    row.value[summary_key] = summary_value
+                row.value.pop("project_summary", None)
+            # If key is in both project_summary and details, details has precedence
+            if 'details' in row.value:
+                for detail_key, detail_value in row.value['details'].iteritems():
+                    row.value[detail_key] = detail_value
+                row.value.pop("details", None)
             projects[row.key] = row.value
 
         return projects
 
+    def list_project_fields(self, undefined=False, project_list=None):
+        # If undefined=True is given, only return fields not in columns defined 
+        # in constants in this module
+        if project_list is None:
+            project_list = self.list_projects()
+        field_items = set()
+        for project_id, value in project_list.iteritems():
+            for key, _ in value.iteritems():
+                field_items.add(key)
+        if undefined:
+            field_items = field_items.difference(set(DEFAULT_COLUMNS.values()))
+            field_items = field_items.difference(set(EXTRA_COLUMNS.values()))
+        return field_items
+
+class ProjectsFieldsDataHandler(ProjectsDataHandler):
+    """ Serves all fields occuring in the values of the ProjectsDataHandler 
+    json object.
+
+    Loaded through /api/v1/projects_fields
+    """
+    def get(self):
+        undefined = self.get_argument("undefined", "False")
+        undefined = (string.lower(undefined) == "true")
+        field_items = self.list_project_fields(undefined)
+        self.write(json.dumps(list(field_items)))
 
 class ProjectDataHandler(tornado.web.RequestHandler):
     """ Serves brief information of a given project.
@@ -91,7 +138,7 @@ class ProjectsHandler(tornado.web.RequestHandler):
     """
     def get(self):
         t = self.application.loader.load("projects.html")
-        self.write(t.generate(other_columns = OTHER_COLUMNS, default_columns = DEFAULT_COLUMNS))
+        self.write(t.generate(columns = COLUMNS))
         
 
 class UppmaxProjectsDataHandler(tornado.web.RequestHandler):
