4567cefa33d3e88c87f1979bafdcf0b37ca27023
Johannes Alneberg <johannesalneberg@gmail.com>
2014-01-23 11:48:20 +0100

    Flowcells list not deprecated

--- status/flowcells.py
+++ status/flowcells.py
@@ -12,7 +12,7 @@ class FlowcellsHandler(SafeHandler):
     """
     def get(self):
         t = self.application.loader.load("flowcells.html")
-        self.write(t.generate(user=self.get_current_user_name(), deprecated=True))
+        self.write(t.generate(user=self.get_current_user_name()))
 
 
 class FlowcellHandler(SafeHandler):
