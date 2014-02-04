56135cf9fe2b8f0822ea723b695def73f6c6c761
Johannes Alneberg <johannesalneberg@gmail.com>
2014-02-04 16:12:16 +0100

    trying oauth

--- status/authorization.py
+++ status/authorization.py
@@ -4,35 +4,21 @@ import json
 
 from status.util import UnsafeHandler
 
-class LoginHandler(tornado.web.RequestHandler, tornado.auth.GoogleMixin):
-    @tornado.web.asynchronous
+class LoginHandler(tornado.web.RequestHandler, tornado.auth.GoogleOAuth2Mixin):
+    @tornado.gen.coroutine
     def get(self):
-        if self.get_argument("openid.mode", None):
-            self.get_authenticated_user(self.async_callback(self._on_auth))
-            return
-        self.authenticate_redirect()
-
-    def _on_auth(self, user):
-        if not user:
-            raise tornado.web.HTTPError(500, "Google auth failed")
-        user_view = self.application.gs_users_db.view("authorized/users", reduce=False)
-        authorized = False
-        for row in user_view[user['email']]:
-            authorized = (row.key == user['email'])
-            break
-        if authorized:
-            self.set_secure_cookie("user", tornado.escape.json_encode(user))
-            self.set_secure_cookie("email", user['email'])
-            url = self.get_argument('next', None)
-            if url:
-                self.redirect(url)
-            else:
-                self.redirect("/")
+        if self.get_argument("code", False):
+            user = yield self.get_authenticated_user(
+                        redirect_uri='https://genomics-status.scilifelab.se/login',
+                        code=self.get_argument('code'))
+            self.set_secure_cookie("user", tornado.escape.json_encode(user))    
         else:
-            url = "/unauthorized?email={0}".format(user['email'])
-            if "name" in user:
-                url += "&name={0}".format(user["name"])
-            self.redirect(url)
+            self.authorize_redirect(
+                        redirect_uri='https://genomics-status.scilifelab.se/login',
+                        client_id=self.application.ouath_key,
+                        scope=['profile', 'email'],
+                        response_type='code',
+                        extra_params={'approval_prompt': 'auto'})
 
 class LogoutHandler(tornado.web.RequestHandler, tornado.auth.GoogleMixin):
     def get(self):
