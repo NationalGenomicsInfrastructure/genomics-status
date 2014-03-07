5a760aaf6f9341bcca1889f641855cea17a2b8a0
Johannes Alneberg <johannes.alneberg@scilifelab.se>
2014-03-07 16:54:32 +0100

    Fixed bug with couchdb key slicing for project summary_dates

--- status/projects.py
+++ status/projects.py
@@ -148,17 +148,18 @@ class ProjectDataHandler(ProjectsBaseDataHandler):
         if not len(view.rows) == 1:
             return {}
 
-        row = view.rows[0]
-        row = self.project_summary_data(summary_row)
-        
+        summary_row = view.rows[0]
+        summary_row = self.project_summary_data(summary_row)
+
         date_view = self.application.projects_db.view("project/summary_dates",
                                                       descending=True,
-                                                      group_level=1)[project]
-        if date_view.rows:
+                                                      group_level=1)
+        date_result = date_view[[project + 'ZZZZ']:[project]]
+        if date_result.rows:
             for date_row in date_view.rows:
                 for date_type, date in date_row.value.iteritems():
-                    row.value[date_type] = date
-        return row.value
+                    summary_row.value[date_type] = date
+        return summary_row.value
 
 
 class ProjectSamplesDataHandler(SafeHandler):
