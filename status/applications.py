d2fbdefab81cde145071f20ee1cdb30d005ad456
Remi-Andre Olsen <remiolsen@remi-andrembp.scilifelab.se>
2014-04-01 14:19:57 +0200

    Changed: From Guillermos code, applications piechart using nvd3

--- status/applications.py
+++ status/applications.py
@@ -74,7 +74,7 @@ class ApplicationsDataHandler(SafeHandler):
     def list_applications(self,start=None,end="z"):
         applications = Counter()
         view = self.application.projects_db.view("project/date_applications")
-        for row in view[[start,""]:[end,"z"]]:
+        for row in view[[start,""]:[end,"z"]]:                
             if row.key[1] is None:
                 # This corresponds to StatusDB:s notation
                 # and avoids conflict with 'None'.
@@ -94,53 +94,8 @@ class ApplicationsPlotHandler(ApplicationsDataHandler):
         end = self.get_argument("end","z")
 
         applications = self.list_applications(start=start,end=end)
+        
         self.set_header("Content-type", "application/json")
-        self.write(applications)
-        #fig = plt.figure(figsize=[10, 8])
-        #ax = fig.add_subplot(111)
-
-        #cmap = plt.cm.prism
-        #colors = cmap(np.linspace(0., 1., len(applications)))
-
-        ##Sort the slices by size (so we know where the thin slices will end up)
-        #applications = sorted(applications.items(), key=lambda x: x[1])
-
-        ##Reorder the slices so not all small slices end up together (causing
-        ##label overlapping)
-        #reordered = []
-        #n_app = len(applications)
-        #for i in range(n_app/2):
-        #    reordered.append(applications[i])
-        #    reordered.append(applications[n_app - 1 - i])
-
-        #applications_labels = [a[0] for a in reordered]
-        #applications_values = [a[1] for a in reordered]
-
-        ##All small labels will be concentrated in the bottom right quarter, in
-        ##order to put these labels horitzontally (and thus avoid overlapping),
-        ##we can start the pie rotated 45.0 degrees
-        #pie_wedge_collection = ax.pie(applications_values,
-        #                              colors=colors,
-        #                              labels=applications_labels,
-        #                              labeldistance=1.05,
-        #                              startangle=45.0)
-
-        ##Set some style parameters
-        #for pie_wedge, pie_texts in zip(pie_wedge_collection[0], pie_wedge_collection[1]):
-        #    pie_wedge.set_edgecolor('white')
-        #    pie_wedge.set_linewidth(1)
-        #    pie_texts.set_fontsize(8)
-
-        #fig.subplots_adjust(left=0.15, right=0.75, bottom=0.15)
-
-        #FigureCanvasAgg(fig)
-
-        #buf = cStringIO.StringIO()
-        #fig.savefig(buf, format="png", bbox_inches='tight', pad_inches=0)
-        #applications = buf.getvalue()
-
-        #self.set_header("Content-Type", "image/png")
-        #self.set_header("Content-Length", len(applications))
         self.write(applications)
 
 
