0f9af72126f82c27a86431983bb986f913e73d29
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-09-25 12:07:26 +0200

    Everything works now

--- run_dir/static/js/suggestion_box.js
+++ run_dir/static/js/suggestion_box.js
@@ -1,10 +1,10 @@
 function fill_suggestions_table() {
   $.getJSON('/api/v1/suggestions', function(data) {
-    $("#suggestionsTable body").empty();
+    $("#suggestionsTableBody").empty();
     $.each(data, function(date, card_info) {
       var card_date = "<tr><td><a target='_blank' href='" + card_info[1] + "'>" + card_info[0] + "</a></td>";
       var card_link = "<td>" + date.split('T')[0] + ' at ' + date.split('T')[1].split('.')[0] + "</td></tr>";
-      $('#suggestionsTable tr:last').after(card_date + card_link);
+      $('#suggestionsTableBody').append(card_date + card_link);
     });
   });
 };
@@ -24,12 +24,31 @@ $.validator.setDefaults({
     error.insertAfter(element);
   }
 });
-$("#suggestionForm").validate({submitHandler: function(form) {
-  $.post('/suggestion_box', $('#suggestionForm').serialize());
-}
-});
 
+//Generate alert when POSTing and refresh suggestions table
+$("#suggestionForm").validate({
+  submitHandler: function(form) {
+    $.post('/suggestion_box', $('#suggestionForm').serialize())
+    .done(function() {
+      $("#alertDiv").addClass('alert-success');
+      $("#alertDiv").append("Card successfully created in Trello!");
+      $("#alertDiv").show();
+    })
+    .fail(function() {
+      $("#alertDiv").addClass('alert-error');
+      $("#alertDiv").append("Something went wrong when creating the card in Trello. Please try it again.");
+      $("#alertDiv").show();
+    })
+    .always(function() {
+      fill_suggestions_table();
+    })
+  }
+});
 
 $(document).ready(function() {
   fill_suggestions_table();
+  // Make the button to hide the alert
+  $('#alertButton').live('click', function(event) {        
+    $('#alertDiv').hide();
+  });
 });
