8036c3d9372ef26a294a4c9f6e5ea610171a9338
Phil Ewels <phil.ewels@scilifelab.se>
2015-06-17 20:08:34 +0200

    Starting on the incoming deliveries..

--- run_dir/static/js/deliveries.js
+++ run_dir/static/js/deliveries.js
@@ -5,7 +5,10 @@ Powers /deliveries/ - template is run_dir/design/deliveries.html
 */
 
 // Static config vars
-var bioinfo_api_url = '/api/v1/bioinfo_analysis/';
+var bioinfo_urls = {
+  'ongoing': '/api/v1/bioinfo_analysis/?status=Ongoing',
+  'incoming': '/api/v1/bioinfo_analysis/?status=Incoming'
+};
 var bioinfo_states = ['Ongoing', 'Delivered', 'Aborted'];
 var bioinfo_states_classes = ['label-warning', 'label-success', 'label-danger'];
 var bioinfo_classes = ['unknown', 'success', 'warning', 'danger', 'active'];
@@ -37,123 +40,127 @@ $(document).ready(function() {
     });
   });
 
-  $.getJSON(bioinfo_api_url, function (data) {
+  $.each(bioinfo_urls, function(delivery_type, bioinfo_api_url){
+    console.log(bioinfo_api_url);
+    $.getJSON(bioinfo_api_url, function (data) {
 
-    // Hide the loading row and build the real runs based on the template
-    var pids = Object.keys(data);
-    if(pids.length == 0){
-      $('#ongoing_deliveries, #incoming_deliveries').html('<div class="alert alert-info">No active deliveries found.</div>');
-    } else {
-      // Get the template HTML
-      var run_template = $('#ongoing_deliveries .ongoing-delivery table tbody tr').detach();
-      var project_template = $('#ongoing_deliveries .ongoing-delivery').detach();
+      // Hide the loading row and build the real runs based on the template
+      var pids = Object.keys(data);
+      if(pids.length == 0){
+        $('#ongoing_deliveries, #incoming_deliveries').html('<div class="alert alert-info">No active deliveries found.</div>');
+      } else {
+        // Get the template HTML
+        var run_template = $('#ongoing_deliveries .ongoing-delivery table tbody tr').detach();
+        var project_template = $('#ongoing_deliveries .ongoing-delivery').detach();
 
-      // Get the project data
-      $.getJSON('/api/v1/projects?list='+pids.join(','), function (pdata) {
+        // Get the project data
+        $.getJSON('/api/v1/projects?list='+pids.join(','), function (pdata) {
 
-        $.each(data, function(pid, runs){
-          var p = project_template.clone();
+          $.each(data, function(pid, runs){
+            var p = project_template.clone();
 
-          // Main project fields
-          p.find('.bi-project-id').text(pid);
-          p.find('.bi-project-name').text(pdata[pid]['project_name']);
-          p.find('.bi-project-application').text(pdata[pid]['application']);
-          p.find('.bi-project-facility').text(pdata[pid]['type']);
-          if(pdata[pid]['type'] == 'Application'){
-            p.find('.bi-project-facility').removeClass('label-primary').addClass('label-success');
-          }
-          p.find('.bi-project-assigned').text(pdata[pid]['bioinfo_responsible']);
-          if (pdata[pid]['latest_running_note'] !== '') {
-            var noteobj = JSON.parse(pdata[pid]['latest_running_note']);
-            var ndate = Object.keys(noteobj)[0];
-            var note = $(make_markdown(noteobj[ndate]['note'])).find('div, p').contents().unwrap();
-            p.find('.bi-project-note').html(note);
-          }
+            // Main project fields
+            p.find('.bi-project-id').text(pid);
+            p.find('.bi-project-name').text(pdata[pid]['project_name']);
+            p.find('.bi-project-application').text(pdata[pid]['application']);
+            p.find('.bi-project-facility').text(pdata[pid]['type']);
+            if(pdata[pid]['type'] == 'Application'){
+              p.find('.bi-project-facility').removeClass('label-primary').addClass('label-success');
+            }
+            p.find('.bi-project-assigned').text(pdata[pid]['bioinfo_responsible']);
+            if (pdata[pid]['latest_running_note'] !== '') {
+              var noteobj = JSON.parse(pdata[pid]['latest_running_note']);
+              var ndate = Object.keys(noteobj)[0];
+              var note = $(make_markdown(noteobj[ndate]['note'])).find('div, p').contents().unwrap();
+              p.find('.bi-project-note').html(note);
+            }
 
-          $.each(runs, function(runid, run){
-            var r = run_template.clone();
-            var flowcell = runid.replace(/_.+_/g, '_');
+            $.each(runs, function(runid, run){
+              var r = run_template.clone();
+              var flowcell = runid.replace(/_.+_/g, '_');
 
-            // Main run fields
-            r.find('.bi-runid samp a').text(runid);
-            r.find('.bi-runid samp a').attr('href', '/flowcells/'+flowcell);
-            r.find('.bi-run-status span').text(run['status']).
-                  removeClass('label-default').
-                  addClass(bioinfo_states_classes[bioinfo_states.indexOf(run['status'])]);
+              // Main run fields
+              r.find('.bi-runid samp a').text(runid);
+              r.find('.bi-runid samp a').attr('href', '/flowcells/'+flowcell);
+              r.find('.bi-run-status span').text(run['status']).
+                    removeClass('label-default').
+                    addClass(bioinfo_states_classes[bioinfo_states.indexOf(run['status'])]);
 
-            // Progress bar
-            var total = passed = warnings = fails = NAs = unsets = 0;
-            var dateregex = new RegExp(/\d{4}-\d{2}-\d{2}/);
-            $.each(run, function(key, val){
+              // Progress bar
+              var total = passed = warnings = fails = NAs = unsets = 0;
+              var dateregex = new RegExp(/\d{4}-\d{2}-\d{2}/);
+              $.each(run, function(key, val){
 
-              var ignore = true;
-              if(app_fields['core'].indexOf(key) > -1){ ignore = false; }
-              if(pdata[pid]['type'] == 'Application'){
-                if(app_fields['applications'].indexOf(key) > -1){ ignore = false; }
-              }
-              $.each(app_classes, function(appclass, apps){
-                if(apps.indexOf(pdata[pid]['application']) > -1){
-                  if(app_fields[appclass].indexOf(key) > -1){
-                    ignore = false;
+                var ignore = true;
+                if(app_fields['core'].indexOf(key) > -1){ ignore = false; }
+                if(pdata[pid]['type'] == 'Application'){
+                  if(app_fields['applications'].indexOf(key) > -1){ ignore = false; }
+                }
+                $.each(app_classes, function(appclass, apps){
+                  if(apps.indexOf(pdata[pid]['application']) > -1){
+                    if(app_fields[appclass].indexOf(key) > -1){
+                      ignore = false;
+                    }
+                  }
+                });
+
+                if(!ignore){
+                  // console.log(key);
+                  total += 1;
+                  switch(val){
+                    case 'Pass': passed += 1; break;
+                    case 'Warning': warnings += 1; break;
+                    case 'Fail': fails += 1; break;
+                    case 'N/A': NAs += 1; break;
+                    case '?': unsets += 1; break;
+                    default: if(dateregex.test(val)){ passed += 1; }
                   }
                 }
               });
+              total -= NAs;
+              r.find('.bi-run-pwf .progress .progress-bar-success').css('width', ((passed / total) * 100)+'%').attr('title', passed+ ' Passes');
+              r.find('.bi-run-pwf .progress .progress-bar-warning').css('width', ((warnings / total) * 100)+'%').attr('title', warnings+ ' Warnings');
+              r.find('.bi-run-pwf .progress .progress-bar-danger').css('width', ((fails / total) * 100)+'%').attr('title', fails+ ' Fails');
+              if(unsets > 0){
+                r.find('.bi-run-pwf').addClass('warning');
+              } else {
+                r.find('.bi-run-pwf').addClass('success');
+              }
 
-              if(!ignore){
-                // console.log(key);
-                total += 1;
-                switch(val){
-                  case 'Pass': passed += 1; break;
-                  case 'Warning': warnings += 1; break;
-                  case 'Fail': fails += 1; break;
-                  case 'N/A': NAs += 1; break;
-                  case '?': unsets += 1; break;
-                  default: if(dateregex.test(val)){ passed += 1; }
+              // Get the flow cell running notes
+              $.getJSON('/api/v1/flowcell_notes/'+flowcell, function (fcrn) {
+                var printnote = '-';
+                if(Object.keys(fcrn).length > 0){
+                  $.each(fcrn, function(date, note){
+                    if(note['note'].indexOf(pid) > -1){
+                      note['note'] = note['note'].replace(pid+': ', '');
+                      printnote = $(make_markdown(note['note'])).find('div, p').contents().unwrap();
+                    }
+                  });
                 }
-              }
-            });
-            total -= NAs;
-            r.find('.bi-run-pwf .progress .progress-bar-success').css('width', ((passed / total) * 100)+'%').attr('title', passed+ ' Passes');
-            r.find('.bi-run-pwf .progress .progress-bar-warning').css('width', ((warnings / total) * 100)+'%').attr('title', warnings+ ' Warnings');
-            r.find('.bi-run-pwf .progress .progress-bar-danger').css('width', ((fails / total) * 100)+'%').attr('title', fails+ ' Fails');
-            if(unsets > 0){
-              r.find('.bi-run-pwf').addClass('warning');
-            } else {
-              r.find('.bi-run-pwf').addClass('success');
-            }
+                r.find('.bi-run-note > span').html(printnote);
+              });
 
-            // Get the flow cell running notes
-            $.getJSON('/api/v1/flowcell_notes/'+flowcell, function (fcrn) {
-              var printnote = '-';
-              if(Object.keys(fcrn).length > 0){
-                $.each(fcrn, function(date, note){
-                  if(note['note'].indexOf(pid) > -1){
-                    note['note'] = note['note'].replace(pid+': ', '');
-                    printnote = $(make_markdown(note['note'])).find('div, p').contents().unwrap();
-                  }
-                });
-              }
-              r.find('.bi-run-note > span').html(printnote);
+              // Add to table
+              p.find('table tbody').append(r);
             });
 
-            // Add to table
-            p.find('table tbody').append(r);
-          });
+            // Add everything to the DOM and show
+            $('#ongoing_deliveries').append(p);
+            $('#loading_spinner').hide();
+            $('#page_content').show();
 
-          // Add everything to the DOM and show
-          $('#ongoing_deliveries').append(p);
-          $('#loading_spinner').hide();
-          $('#page_content').show();
+          }); // loop through runs
+        }); // Get project data
 
-        }); // loop through runs
-      }); // Get project data
-
-    }
+      }
 
 
-  }).fail(function( jqxhr, textStatus, error ) {
-    $('#loading_spinner').hide();
-    $('#page_content').show().html('<h1>Error - Delivery Information Not Loaded</h1><div class="alert alert-danger">Oops! Sorry about that, we can\'t load the deliveries info. Please try again later or contact one of the genomics status team.</div>');
+    }).fail(function( jqxhr, textStatus, error ) {
+      $('#loading_spinner').hide();
+      $('#page_content').show().html('<h1>Error - Delivery Information Not Loaded</h1><div class="alert alert-danger">Oops! Sorry about that, we can\'t load the deliveries info. Please try again later or contact one of the genomics status team.</div>');
+    });
+
   });
 
 });
