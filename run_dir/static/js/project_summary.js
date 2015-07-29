f49fca325896bcf89af99425f865c5ffb7f7052f
Denis Moreno <denis.moreno@scilifelab.se>
2015-07-29 14:45:44 +0200

    cleaning the message

--- run_dir/static/js/project_summary.js
+++ run_dir/static/js/project_summary.js
@@ -11,6 +11,7 @@ $(document).ready(function() {
 function upload_sample_udfs(process_id, sample_list){
     final_obj={};
     sl=sample_list.split(",");
+    $("#update_status_value").html('Updating Samples, please wait... <br />');
     for (s_id in sl){
         $('.'+process_id+'_sample_name').each(function(idx, el){
             udf_key=el.innerHTML.replace(/ /g, "_").replace(/\(/, '\\\(').replace(/\)/, '\\\)');
@@ -26,6 +27,7 @@ function upload_sample_udfs(process_id, sample_list){
 
 function upload_project_summary_udfs(process_id){
     final_obj={};
+    $("#update_status_value").html('Updating Process, please wait... <br />');
     $('.step_'+process_id+'_name').each(function(idx, el){
         udf_key=el.innerHTML.replace(/ /g, "_");
         $('.step_'+process_id+'_value_'+udf_key).each(function(idx2, el2){
@@ -64,4 +66,4 @@ function clean_object(obj){
     }
     return obj
 
-}
+}Samples
