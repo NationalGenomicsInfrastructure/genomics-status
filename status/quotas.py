7586c7a9b14518d1e37ff3a4007f259978585930
Kate Stepanova <ekaterina.stepanova@scilifelab.se>
2016-06-29 16:21:05 +0200

    Uppmax quotas: display cpu quota decrease

--- status/quotas.py
+++ status/quotas.py
@@ -12,6 +12,7 @@ class QuotasHandler(SafeHandler):
     """ Serves a grid of time series plots for UPPNEX storage quotas.
     URL: /quotas
     """
+
     def get(self):
         view = self.application.server_status_db.view("uppmax/by_timestamp")
         uppmax_projects = {}
@@ -21,67 +22,60 @@ class QuotasHandler(SafeHandler):
 #          # in javascript it has to be multiplied by 1000, no idea why
             timestamp = int(time.mktime(parser.parse(row.value.get('time')).timetuple())) * 1000
             if project_id not in uppmax_projects:
-                uppmax_projects[project_id] = {'cpu_hours': {'plot_data': [], 'limit_data': [], 'max_x_value': 0, 'min_time': timestamp},
-                    'disk_usage': {'plot_data': [], 'limit_data': [], 'max_x_value': 0, 'min_time': timestamp},
-                    'nobackup_usage': {'plot_data': [], 'limit_data': [], 'max_x_value': 0, 'min_time': timestamp},
+                uppmax_projects[project_id] = {
+                    'cpu_hours':        {'plot_data': [], 'limit_data': [], 'max_x_value': 0, 'min_time': timestamp},
+                    'disk_usage':       {'plot_data': [], 'limit_data': [], 'max_x_value': 0, 'min_time': timestamp},
+                    'nobackup_usage':   {'plot_data': [], 'limit_data': [], 'max_x_value': 0, 'min_time': timestamp},
                     'quota_decrease': {},
-                    'nobackup_quota_decrease': {}}
+                    'nobackup_quota_decrease': {},
+                    'cpu_quota_decrease': {}}
 
-            quota_decrease = row.value.get('quota_decrease')
-            # sometimes it is  '*', skip it
-            if quota_decrease and '@' in quota_decrease:
-                quota_decrease = quota_decrease.split(',')
-                for value in quota_decrease:
-                    try:
-                        quota, date = value.strip().split('@')
-                    except:
-                        continue
-                    today = datetime.date.today()
-                    quota_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
-                    if quota_date < today:
-                        continue
-                    days = (quota_date - today).days
-                    if 'nobackup' in project_nobackup:
-                        uppmax_projects[project_id]['nobackup_quota_decrease'] = {'date': date, 'quota': quota, 'days': days }
-                    else:
-                        uppmax_projects[project_id]['quota_decrease'] = {'date': date, 'quota': quota, 'days': days }
-            if project_id == project_nobackup: # project_nobackup = 'a2010002_nobackup'
-                try:
-                    disk_usage = [timestamp, float(row.value['usage (GB)'])]
-                    disk_limit = [timestamp, float(row.value['quota limit (GB)'])]
-                except:
-                    # if we have only cpu hours values (or just nothing)
-                    continue
-                uppmax_projects[project_id]['disk_usage']['plot_data'].append(disk_usage)
-                uppmax_projects[project_id]['disk_usage']['limit_data'].append(disk_limit)
-                min_time = uppmax_projects[project_id]['disk_usage']['min_time']
-                uppmax_projects[project_id]['disk_usage']['min_time'] = min(min_time, timestamp)
-                max_x_value = uppmax_projects[project_id]['disk_usage']['max_x_value']
-                uppmax_projects[project_id]['disk_usage']['max_x_value'] = max(max_x_value, disk_limit[1], disk_usage[1])
+            # get quota decrease
+            quota_decrease = self.__extract_quota_decrease(row)
+            if quota_decrease:
+                if 'nobackup' in project_nobackup:
+                    uppmax_projects[project_id]['nobackup_quota_decrease'] = quota_decrease
+                else:
+                    uppmax_projects[project_id]['quota_decrease'] = quota_decrease
 
-                try:
-                    cpu_usage = [timestamp, float(row.value['cpu hours'])]
-                    cpu_limit = [timestamp, float(row.value['cpu limit'])]
-                except:
-                    continue
-                uppmax_projects[project_id]['cpu_hours']['plot_data'].append(cpu_usage)
-                uppmax_projects[project_id]['cpu_hours']['limit_data'].append(cpu_limit)
-                min_time = uppmax_projects[project_id]['cpu_hours']['min_time']
-                uppmax_projects[project_id]['cpu_hours']['min_time'] = min(min_time, timestamp)
-                max_x_value = uppmax_projects[project_id]['cpu_hours']['max_x_value']
-                uppmax_projects[project_id]['cpu_hours']['max_x_value'] = max(max_x_value, cpu_limit[1], cpu_usage[1])
+            # get cpu quota decrease
+            cpu_quota_decrease = self.__extract_cpu_quota_decrease(row)
+            if cpu_quota_decrease:
+                uppmax_projects[project_id]['cpu_quota_decrease'] = cpu_quota_decrease
+
+            # get disk usage
+            try:
+                disk_usage = float(row.value['usage (GB)'])
+                disk_limit = float(row.value['quota limit (GB)'])
+            except:
+                # if no usage, or if it's not a float
+                pass
             else:
-                try:
-                    nobackup_usage = [timestamp, float(row.value['usage (GB)'])]
-                    nobackup_limit = [timestamp, float(row.value['quota limit (GB)'])]
-                except:
-                    continue
-                uppmax_projects[project_id]['nobackup_usage']['plot_data'].append(nobackup_usage)
-                uppmax_projects[project_id]['nobackup_usage']['limit_data'].append(nobackup_limit)
-                min_time = uppmax_projects[project_id]['nobackup_usage']['min_time']
-                uppmax_projects[project_id]['nobackup_usage']['min_time'] = min(min_time, timestamp)
-                max_x_value = uppmax_projects[project_id]['nobackup_usage']['max_x_value']
-                uppmax_projects[project_id]['nobackup_usage']['max_x_value'] = max(max_x_value, nobackup_limit[1], nobackup_usage[1])
+                usage_plot_data = [timestamp, disk_usage]
+                limit_plot_data = [timestamp, disk_limit]
+                if 'nobackup' in project_nobackup:
+                    project_data = uppmax_projects[project_id]['nobackup_usage']
+                else:
+                    project_data = uppmax_projects[project_id]['disk_usage']
+
+                project_data['plot_data'].append(usage_plot_data)
+                project_data['limit_data'].append(limit_plot_data)
+                project_data['max_x_value'] = max(project_data['max_x_value'], disk_usage, disk_limit)
+                project_data['min_time'] = min(project_data['min_time'], timestamp)
+
+            # get cpu hours
+            try:
+                cpu_usage = float(row.value['cpu hours'])
+                cpu_limit = float(row.value['cpu limit'])
+            except:
+                pass
+            else:
+                usage_plot_data = [timestamp, cpu_usage]
+                limit_plot_data = [timestamp, cpu_limit]
+                project_data = uppmax_projects[project_id]['cpu_hours']
+                project_data['plot_data'].append(usage_plot_data)
+                project_data['limit_data'].append(limit_plot_data)
+                project_data['max_x_value'] = max(project_data['max_x_value'], cpu_usage, cpu_limit)
 
         for project_id in uppmax_projects:
             description = self.application.uppmax_projects.get(project_id, '')
@@ -90,3 +84,39 @@ class QuotasHandler(SafeHandler):
         t = self.application.loader.load("uppmax_quotas.html")
         self.write(t.generate(gs_globals=self.application.gs_globals, user=self.get_current_user_name(),
                               uppmax_projects=uppmax_projects))
+
+    def __extract_quota_decrease(self, row):
+        result = []
+        quota_decrease = row.value.get('quota_decrease')
+        # sometimes it is  '*', skip it
+        if quota_decrease and '@' in quota_decrease:
+            quota_decrease = quota_decrease.split(',')
+            today = datetime.date.today()
+            for value in quota_decrease:
+                try:
+                    quota, date = value.strip().split('@')
+                except:
+                    continue
+                quota_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+                if quota_date < today:
+                    continue
+                days = (quota_date - today).days
+                result.append({'date': date, 'quota': quota, 'days': days })
+
+        next_decrease = min(result, key=lambda d:d['date']) if result else {}
+        return next_decrease
+
+    def __extract_cpu_quota_decrease(self, row):
+        result = []
+        quota_decrease = row.value.get('cpu_quota_decrease')
+        if quota_decrease:
+            today = datetime.date.today()
+            for date, quota in quota_decrease.items():
+                print date, quota
+                quota_date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+                if quota_date < today:
+                    continue
+                days = (quota_date - today).days
+                result.append({'date': quota_date, 'quota': quota, 'days': days})
+        next_decrease = min(result, key=lambda d:d['date']) if result else {}
+        return next_decrease
