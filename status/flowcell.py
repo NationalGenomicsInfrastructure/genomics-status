2310f2396cb167989df5d6c4aa3ac274f993bdea
Kate Stepanova <ekaterina.stepanova@scilifelab.se>
2016-08-09 16:12:56 +0200

    Flowcell page: add thousand groups

--- status/flowcell.py
+++ status/flowcell.py
@@ -29,7 +29,7 @@ class FlowcellHandler(SafeHandler):
                                   user=self.get_current_user_name(),
                                   error="Oops! Sorry about that, we can't find the flow cell {}".format(flowcell_id)))
             return
-        
+
         tresholds = {
             'HiSeq X': 320,
             'RapidHighOutput': 188,
