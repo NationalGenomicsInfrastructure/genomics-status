5c269ff3efd8ea61a70ad39027115baa7fa120f0
Phil Ewels <phil.ewels@scilifelab.se>
2016-09-07 13:44:31 +0200

    Reduce the application names list in the stats API
    This code is horrible. I expect Denis will want to rewrite it all and that's fine. If not, it works the way I want it to at least.

--- status/statistics.py
+++ status/statistics.py
@@ -1,4 +1,5 @@
 import json
+import yaml
 
 from status.util import SafeHandler, UnsafeHandler
 
@@ -22,19 +23,55 @@ def get_stats_data(db,view,gl=None):
             data[row.key]=row.value
     return data
 
+def clean_application_keys_y(raw_data):
+    # Normalise Application names to approved list
+    # Don't let Denis see this, it will make him cry
+    with open('application_keys.yaml') as f:
+        app_cats=yaml.load(f)
+    data={}
+    for year in raw_data:
+        data[year] = {}
+        for c in raw_data[year]:
+            try:
+                data[year][app_cats[c]] += raw_data[year][c]
+            except KeyError:
+                try:
+                    data[year][app_cats[c]] = raw_data[year][c]
+                except KeyError:
+                    data[year][c] = raw_data[year][c]
+    return data
+
+def clean_application_keys(raw_data):
+    # Normalise Application names to approved list
+    # Don't let Denis see this, it will make him cry
+    with open('application_keys.yaml') as f:
+        app_cats=yaml.load(f)
+    data={}
+    for c in raw_data:
+        try:
+            data[app_cats[c]] += raw_data[c]
+        except KeyError:
+            try:
+                data[app_cats[c]] = raw_data[c]
+            except KeyError:
+                data[c] = raw_data[c]
+    return data
+
 
 class YearApplicationsProjectHandler(SafeHandler):
     def get(self):
-        data={}
-        data=get_stats_data(self.application.projects_db, "genomics-dashboard/year_application_count", 2)
+        raw_data={}
+        raw_data=get_stats_data(self.application.projects_db, "genomics-dashboard/year_application_count", 2)
+        data = clean_application_keys_y(raw_data)
         self.set_header('Content-Type', 'application/json')
         self.set_status(200)
         self.write(json.dumps(data))
 
 class YearApplicationsSamplesHandler(SafeHandler):
     def get(self):
-        data={}
-        data=get_stats_data(self.application.projects_db, "genomics-dashboard/year_application_count_samples", 2)
+        raw_data={}
+        raw_data=get_stats_data(self.application.projects_db, "genomics-dashboard/year_application_count_samples", 2)
+        data = clean_application_keys_y(raw_data)
         self.set_header('Content-Type', 'application/json')
         self.set_status(200)
         self.write(json.dumps(data))
@@ -57,16 +94,18 @@ class YearDeliverytimeProjectsHandler(SafeHandler):
 
 class ApplicationOpenProjectsHandler(SafeHandler):
     def get(self):
-        data={}
-        data=get_stats_data(self.application.projects_db, "genomics-dashboard/open_application_count_projects", 1)
+        raw_data={}
+        raw_data=get_stats_data(self.application.projects_db, "genomics-dashboard/open_application_count_projects", 1)
+        data = clean_application_keys(raw_data)
         self.set_header('Content-Type', 'application/json')
         self.set_status(200)
         self.write(json.dumps(data))
 
 class ApplicationOpenSamplesHandler(SafeHandler):
     def get(self):
-        data={}
-        data=get_stats_data(self.application.projects_db, "genomics-dashboard/open_application_count_samples", 1)
+        raw_data={}
+        raw_data=get_stats_data(self.application.projects_db, "genomics-dashboard/open_application_count_samples", 1)
+        data = clean_application_keys(raw_data)
         self.set_header('Content-Type', 'application/json')
         self.set_status(200)
         self.write(json.dumps(data))
@@ -93,9 +132,15 @@ class StatsAggregationHandler(UnsafeHandler):
             }
 
     def get(self):
+        clean_appkeys_y=['num_projects','num_samples']
+        clean_appkeys=['open_projects','open_project_samples']
         data={}
         for pa in self.project_aggregates:
             data[pa]=get_stats_data(self.application.projects_db, self.project_aggregates[pa][0], self.project_aggregates[pa][1])
+            if pa in clean_appkeys_y:
+                data[pa] = clean_application_keys_y(data[pa])
+            if pa in clean_appkeys:
+                data[pa] = clean_application_keys(data[pa])
         for fa in self.flowcell_aggregates:
             data[fa]=get_stats_data(self.application.x_flowcells_db, self.flowcell_aggregates[fa][0], self.flowcell_aggregates[fa][1])
         self.set_header('Content-Type', 'application/json')
