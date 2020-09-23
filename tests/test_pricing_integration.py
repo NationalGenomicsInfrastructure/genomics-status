437d006dc0a68c8e1ed07e8f7318a0fa2860a046
Johannes Alneberg <alneberg@kth.se>
2020-09-23 16:00:12 +0200

    Tests for changing exchange rates

--- tests/test_pricing_integration.py
+++ tests/test_pricing_integration.py
@@ -2,19 +2,21 @@ import time
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
-class TestAddproduct(unittest.TestCase):
+class TestAddProduct(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.get("http://localhost:9761/pricing_quote")
+        self.driver.set_window_size(1920, 1080)
         time.sleep(1)
 
     def tearDown(self):
         self.driver.quit()
 
     def test_addproduct(self):
-        self.driver.set_window_size(1920, 1080)
+        """Adding and removing product items to the quote, checking the counts in the input box. """
         self.driver.find_element(By.CSS_SELECTOR, ".status_available:nth-child(2) .add-to-quote .glyphicon").click()
         self.driver.find_element(By.CSS_SELECTOR, ".status_available:nth-child(5) .add-to-quote .glyphicon").click()
 
@@ -42,3 +44,45 @@ class TestAddproduct(unittest.TestCase):
         self.driver.find_element(By.CSS_SELECTOR, ".quote-product-list li:nth-child(1) .glyphicon").click()
         elements = self.driver.find_elements(By.CSS_SELECTOR, ".quote-product-list li > .quote_product_name")
         self.assertEqual(len(elements), 0, msg="Removing the last product should leave no product in the quote")
+
+    def test_exchangerates(self):
+        """Switching between two known exchange rate dates from _statusdb_dev_"""
+        self.driver.find_element(By.LINK_TEXT, "(Change)").click()
+        time.sleep(0.5)
+
+        self.driver.find_element(By.ID, "datepicker").click()
+        self.driver.find_element(By.ID, "datepicker").clear()
+        self.driver.find_element(By.ID, "datepicker").send_keys("2020-09-15")
+        self.driver.find_element(By.CSS_SELECTOR, ".modal-body > p").click()
+        self.driver.find_element(By.ID, "datepicker-btn").click()
+        time.sleep(1.0)
+
+        usd_rate = self.driver.find_element_by_id('exch_rate_usd').text
+        self.assertEqual(usd_rate, '8.75', msg="Matching the known rate")
+
+        # Add bunch of products
+        for i in range(6):
+            self.driver.find_element(By.CSS_SELECTOR, ".status_available:nth-child({}) .add-to-quote .glyphicon".format(i+1)).click()
+        time.sleep(1.0)
+
+        current_price = self.driver.find_elements(By.CSS_SELECTOR, ".quote_totals .quote_totals_val.quote_sweac")[0].text
+        current_price_int = int(current_price.split(' ')[0])  # Get number from e.g. '8421 SEK'
+
+        self.driver.execute_script("window.scrollTo(0, 0)")
+        self.driver.find_element(By.LINK_TEXT, "(Change)").click()
+        time.sleep(0.5)
+
+        self.driver.find_element(By.ID, "datepicker").click()
+        self.driver.find_element(By.ID, "datepicker").clear()
+        self.driver.find_element(By.ID, "datepicker").send_keys("2020-07-01")
+        self.driver.find_element(By.CSS_SELECTOR, ".modal-body > p").click()
+        self.driver.find_element(By.ID, "datepicker-btn").click()
+        time.sleep(1.0)
+
+        usd_rate = self.driver.find_element_by_id('exch_rate_usd').text
+        self.assertEqual(usd_rate, '9.63', msg="Matching the known rate")
+
+        new_price = self.driver.find_elements(By.CSS_SELECTOR, ".quote_totals .quote_totals_val.quote_sweac")[0].text
+        new_price_int = int(new_price.split(' ')[0])  # Get number from e.g. '8421 SEK'
+
+        self.assertGreater(new_price_int, current_price_int, msg="Weaker SEK should lead to higher price")
