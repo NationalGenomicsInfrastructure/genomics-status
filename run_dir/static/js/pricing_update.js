414173c0658c54dcf848a0e6d2ae41965ef5ba9c
Johannes Alneberg <alneberg@kth.se>
2020-10-29 14:08:50 +0100

    Basic modal for alternative components with Vue.js

--- run_dir/static/js/pricing_update.js
+++ run_dir/static/js/pricing_update.js
@@ -5,6 +5,23 @@ $( document ).ready(function() {
       translate_component_ids(e);
   });
 
+  /* Plain javascript style and no jquery since it's from bootstrap docs */
+  var componentsModal = document.getElementById('componentsModal')
+  componentsModal.addEventListener('show.bs.modal', function (event) {
+    // Button that triggered the modal
+    var button = event.relatedTarget
+    // Extract info from data-* attributes
+    var product_id = button.getAttribute('data-product')
+
+    var product = global_products_json[product_id]
+    // If necessary, you could initiate an AJAX request here
+    // and then do the updating in a callback.
+    //
+    // Update the modal's content.
+    var modalTitle = componentsModal.querySelector('.modal-title')
+
+    modalTitle.textContent = 'Edit components for ' + product.REF_ID
+  })
 });
 
 function translate_component_ids(element) {
@@ -18,27 +35,78 @@ function translate_component_ids(element) {
     }
 }
 
+const all_products_reactive = Vue.reactive(global_products_json)
+const all_components_reactive = Vue.reactive(global_components_json)
+
 const ProductForm = {
     data() {
         return {
-            all_components: global_components_json,
-            all_products: global_products_json,
+            all_components: all_components_reactive,
+            all_products: all_products_reactive,
+            modal_product_id: "52" //Should probably be null when we handle that edge case
+        }
+    },
+    methods: {
+        showModalFn(event) {
+            if (event) {
+                this.modal_product_id = event.target.dataset.productId
+            }
+            var myModal = new bootstrap.Modal(document.getElementById('myModal'))
+            myModal.show()
         }
     }
 }
 
 const app = Vue.createApp(ProductForm)
 
+app.component('modal-component', {
+    computed: {
+        product() {
+            return this.$parent.all_products[this.$parent.modal_product_id]
+        },
+        altComponentIds() {
+            return this.product['Alternative Components']
+        },
+        altComponents() {
+            var components = new Array();
+            for (comp_id in this.altComponentIds) {
+                components.push(this.$parent.all_components[comp_id])
+            }
+            return components
+        }
+      },
+    template:
+        `   <div class="modal-header">
+              <h4 class="modal-title">Edit components for {{this.product.REF_ID}}</h4>
+              <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="row">
+                  <h5>Selected Components</h5>
+                  <p>{{this.altComponentIds}}</p>
+                </div>
+                <div class="row">
+                  <h5>All components</h5>
+                  <template v-for="(component, component_id) in this.$parent.all_components">
+                    {{ component['Product name']}}<br>
+                  </template>
+                </div>
+              </form>
+            </div>
+        `
+})
+
 app.component('alt-component-item', {
     template: `<template v-for="component in altComponents">
-                {{component["Product name"]}}<br>
-                </template>`,
+                 {{component["Product name"]}}<br>
+               </template>`,
     computed: {
         product() {
             return this.$parent.all_products[this.product_id]
         },
         altComponentIds() {
-            return this.$parent.all_products[this.product_id]['Alternative Components']
+            return this.product['Alternative Components']
         },
         altComponents() {
             var components = new Array();
