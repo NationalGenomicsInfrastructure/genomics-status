4bff8aa52f4bd8adb6f18612b62de3d0f58b9ea2
FranBonath <franziska.bonath@scilifelab.se>
2020-10-30 11:17:21 +0100

    added comments, corrected regex for projectID, improved readability

--- status/barcode.py
+++ status/barcode.py
@@ -17,15 +17,25 @@ class BarcodeHandler(SafeHandler):
             - already existing barcode file
             - text file
             - text input
+
+        The second part prints user labels with barcodes to be send out together with
+        plates. The options are as follows:
+            - plate number start. i.e. P1
+            - plate number end. i.e. P5
+            - number of projects
+        Separate labels will be printed for the range between plate number start and plate
+        number end. Further, if more than one project is to be printed, the project number
+        increases by one and the choses plate numbers will be printed for the additional
+        projects
     """
     def get(self):
-        t = self.application.loader.load("barcode.html")
+        t = self.application.loader.load('barcode.html')
         self.write(t.generate(gs_globals=self.application.gs_globals, user=self.get_current_user()))
 
     def post(self):
         # some variable will only be attempted to be read if a specific form is submitted
-        labelType = self.get_argument("formType")
-        if(labelType == "lab_labels"):
+        labelType = self.get_argument('formType')
+        if(labelType == 'lab_labels'):
             # how often is the label(s) to be printed? Default: 1
             copies = int(self.get_argument('copies'))
 
@@ -33,7 +43,7 @@ class BarcodeHandler(SafeHandler):
             try: # figure out if a file was entered in the form
                 file_for_printing = self.request.files['file_to_print']
             except KeyError: # form entry for file was empty
-                print("no file was given")
+                print('no file was given')
             else: #form entry for file was not empty
                 #retrieve file and transform the contained text to string format with utf-8 encoding
                 linesToPrint = str(file_for_printing[0]['body'], encoding='utf-8')
@@ -53,24 +63,21 @@ class BarcodeHandler(SafeHandler):
 
             # printing from string input
             text_for_printing = self.get_argument('text_to_print') # retrieves string from form
-            withBarcode = self.get_argument('print_with_barcode') #retrieves information is the label should be printed with barcodes
-            if(withBarcode == "noBC"):
-                barcode = False
-            else:
-                barcode = True
             if len(text_for_printing) > 0 : # check wether there was an entry or not
-                createdLabel = makeBarcode(text_for_printing, match_barcode(text_for_printing, barcode))
+                if self.get_argument('print_with_barcode', '') : # is true when barcode box is ticked
+                    createdLabel = makeBarcode(text_for_printing, match_barcode(text_for_printing, True))
+                else :
+                    createdLabel = makeBarcode(text_for_printing, match_barcode(text_for_printing, False))
                 createdLabel_joined = '\n'.join(createdLabel)
                 for _ in range(copies): # loops over copies to print
                     print_barcode(createdLabel_joined)
             else:
-                print("no text was given")
+                print('no text was given')
 
-            self.set_status(200)
-            self.set_header("Content-type", "application/json")
-            self.finish({'message': 'Submitted to printer!'})
+            statusType = 200
+            message = {'message': 'Submitted to printer!'}
 
-        elif (labelType == "user_labels"):
+        elif (labelType == 'user_labels'):
             # printing user project Labels
             user_project_ID = self.get_argument('projectLabel_to_print')
             startP = self.get_argument('plate_start') # choose the plate number to start, default = 1
@@ -86,59 +93,55 @@ class BarcodeHandler(SafeHandler):
                     projectNo_only_extracted = projectNo_only.group(1) # have only the number of the project ID
                     for projects in range(0,int(projectNo)):
                         new_projectNo = int(projectNo_only_extracted) + projects
-                        new_projectID = "P" + str(new_projectNo)
+                        new_projectID = 'P' + str(new_projectNo)
                         for plate in range(int(startP),(int(endP)+1)) :
-                            new_projectID_plate = new_projectID + "P" + str(plate) # adding the plate number
+                            new_projectID_plate = new_projectID + 'P' + str(plate) # adding the plate number
                             project_barcode = makeBarcode(new_projectID_plate, True)
                             createdProjectLabel_joined = '\n'.join(project_barcode)
-                            print_barcode(createdProjectLabel_joined)
-
-                    self.set_status(200)
-                    self.set_header("Content-type", "application/json")
-                    self.finish({'message': 'Submitted to printer!'})
-
+                    statusType = 200
+                    message = {'message': 'Submitted to printer!'}
                 else:
-                    self.set_status(400)
-                    self.set_header("Content-type", "application/json")
-                    self.finish({'Error': 'please enter a valid user ID, it should start with \"P\", followed by numbers (e.g. P12345)'})
-
+                    statusType = 400
+                    message = {'Error': 'please enter a valid user ID, it should start with \"P\", followed by numbers (e.g. P12345)'}
             else:
-                self.set_status(400)
-                self.set_header("Content-type", "application/json")
-                self.finish({'Error': 'no user project ID was given'})
+                statusType = 400
+                message = {'Error': 'no user project ID was given'}
+        self.set_status(statusType)
+        self.set_header('Content-type', 'application/json')
+        self.finish(message)
 
 def makeBarcode(label, print_bc):
     # prints the formated label to be piped to the barcode printer
     formattedLabel = []
-    formattedLabel.append("^XA") # start of label
-    formattedLabel.append("^DFFORMAT^FS") # download and store format, name of format, end of field data (FS = field stop)
-    formattedLabel.append("^LH0,0") # label home position (label home = LH)
+    formattedLabel.append('^XA') # start of label
+    formattedLabel.append('^DFFORMAT^FS') # download and store format, name of format, end of field data (FS = field stop)
+    formattedLabel.append('^LH0,0') # label home position (label home = LH)
     if print_bc:
-        ch_size = "20"
-        xpositionText = 360
+        ch_size = '20'
+        xpositionText = '360'
         if len(label) > 10:
-            ch_size = "10" # squeezes the text for long texts
-            xpositionText = 440 # moves the text position because the bc is longer
-        formattedLabel.append("^FO{0},35^AFN,60,{1}^FN1^FS".format(xpositionText, ch_size)) # AF = assign font F, field number 1 (FN1), print text at position field origin (FO) rel. to home
-        formattedLabel.append("^FO80,35^BCN,70,N,N^FN2^FS") # BC=barcode 128, field number 2, normal orientation, height 70, no interpretation line.
+            ch_size = '10' # squeezes the text for long texts
+            xpositionText = '440' # moves the text position because the bc is longer
+        formattedLabel.append('^FO{0},35^AFN,60,{1}^FN1^FS'.format(xpositionText, ch_size)) # AF = assign font F, field number 1 (FN1), print text at position field origin (FO) rel. to home
+        formattedLabel.append('^FO80,35^BCN,70,N,N^FN2^FS') # BC=barcode 128, field number 2, normal orientation, height 70, no interpretation line.
     else:
-        ch_size = "40"
-        yposition = 42
+        ch_size = '40'
+        yposition = '42'
         if len(label) > 32:
-            ch_size = 24
-            yposition = 50
+            ch_size = '24'
+            yposition = '50'
         elif len(label) < 20:
-            ch_size = 50
-            yposition = 35
+            ch_size = '50'
+            yposition = '35'
         # Scalable font ^A0N,32,32 should fit roughly 42 chars on our current labels
-        formattedLabel.append("^FO20,{0}^A0N,{1},{1}^FB640,1,0,C,0^FN1^FS".format(yposition, ch_size)) # FO = x,y relative field origin; A0N = scalable font height,width; FB = make into one line field block and center
-    formattedLabel.append("^XZ") # end format
-    formattedLabel.append("^XA") # start of label format
-    formattedLabel.append("^XFFORMAT^FS") # label home posision
-    formattedLabel.append("^FN1^FD{}^FS".format(label)) # this is readable
+        formattedLabel.append('^FO20,{0}^A0N,{1},{1}^FB640,1,0,C,0^FN1^FS'.format(yposition, ch_size)) # FO = x,y relative field origin; A0N = scalable font height,width; FB = make into one line field block and center
+    formattedLabel.append('^XZ') # end format
+    formattedLabel.append('^XA') # start of label format
+    formattedLabel.append('^XFFORMAT^FS') # label home posision
+    formattedLabel.append('^FN1^FD{}^FS'.format(label)) # this is readable
     if print_bc:
-        formattedLabel.append("^FN2^FD{}^FS".format(label)) # this is the barcode
-    formattedLabel.append("^XZ")
+        formattedLabel.append('^FN2^FD{}^FS'.format(label)) # this is the barcode
+    formattedLabel.append('^XZ')
     return(formattedLabel)
 
 def match_barcode(string_to_match, default_value):
@@ -153,7 +156,7 @@ def match_barcode(string_to_match, default_value):
 
 def match_userID(string_to_match):
     #identifies plate IDs as created by LIMS and returns boolean
-    userID_match = re.compile(r'P\d+$')
+    userID_match = re.compile(r'^P\d+$')
     match = re.search(userID_match, string_to_match)
     if match:
         isUserID = True
@@ -163,9 +166,9 @@ def match_userID(string_to_match):
 
 def print_barcode(barcodeFile):
     print('Ready to call lp for printing.')
-    lp_args = ["lp"]
-    lp_args.extend(["-d", "Zebra"])
-    lp_args.append("-")  # lp accepts stdin if '-' is given as filename
+    lp_args = ['lp']
+    lp_args.extend(['-d', 'Zebra'])
+    lp_args.append('-')  # lp accepts stdin if '-' is given as filename
     sp = subprocess.Popen(lp_args,
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
