593bc28cd0985ddf720461dc612c5bdf62621a95
Johannes Alneberg <alneberg@kth.se>
2021-02-04 10:51:03 +0100

    Broken delete button and indentation

--- run_dir/static/js/pricing_preview.js
+++ run_dir/static/js/pricing_preview.js
@@ -87,11 +87,12 @@ app.component('pricing-preview', {
                   <span id="page_title">New Cost Calculator</span>
                   <a class="btn btn-lg ml-5" :class="draft_locked_by_someone_else ? 'btn-secondary disabled' : 'btn-primary'" href="/pricing_update"><i class="fas fa-edit mr-2"></i>Edit</a>
                   <button class="btn btn-lg ml-2" :class="draft_locked_by_someone_else ? 'btn-secondary disabled' : 'btn-success'" data-toggle="modal" data-target="#publish_draft_modal"><i class="far fa-paper-plane mr-2"></i>Publish</button>
+                  <button class="btn btn-lg ml-2" :class="draft_locked_by_someone_else ? 'btn-danger disabled' : 'btn-danger'" data-toggle="modal" data-target="#delete_draft_modal"><i class="fas fa-trash-alt mr-2"></i>Delete</button>
                 </h1>
                 <p>
-                <span class="fw-bold">Last modified</span><span> by {{this.$root.draft_cost_calculator["Last modified by user"]}} at </span><span class="fst-italic"> {{draft_last_modified_at}}</span>
-                ---
-                <span class="fw-bold">Version {{this.$root.published_cost_calculator["Version"]}} was published </span><span> by {{this.$root.published_cost_calculator["Issued by user"]}} at </span><span class="fst-italic"> {{published_at}}</span>
+                  <span class="fw-bold">Last modified</span><span> by {{this.$root.draft_cost_calculator["Last modified by user"]}} at </span><span class="fst-italic"> {{draft_last_modified_at}}</span>
+                  ---
+                  <span class="fw-bold">Version {{this.$root.published_cost_calculator["Version"]}} was published </span><span> by {{this.$root.published_cost_calculator["Issued by user"]}} at </span><span class="fst-italic"> {{published_at}}</span>
                 </p>
                 <template v-if="draft_locked_by_someone_else">
                   <a class="btn btn-danger" @click="reassign_lock"><i class="fas fa-user-lock"></i> Reassign lock to you</a>
@@ -113,56 +114,52 @@ app.component('pricing-preview', {
                   </template>
                 </div>
               </div>
-          </template>
-          <template v-else>
-            <h1>
-              <span id="page_title">New Cost Calculator</span>
-            </h1>
-            <p>
-            <span class="fw-bold">Version {{this.$root.published_cost_calculator["Version"]}} was published </span><span> by {{this.$root.published_cost_calculator["Issued by user"]}} at </span><span class="fst-italic"> {{published_at}}</span>
-            </p>
-            <p> No draft cost calculator exists. </p>
-            <p><button class="btn btn-success" @click="create_new_draft"><i class="fas fa-user-lock"></i> Create new draft</button></p>
-          </template>
-        </div>
-        <template v-if="draft_exists">
-          <div class="row">
-            <div class="col-12">
-              <v-draft-changes-list :modal="false"/>
-            </div>
+            </template>
+            <template v-else>
+              <h1>
+                <span id="page_title">New Cost Calculator</span>
+              </h1>
+              <p>
+              <span class="fw-bold">Version {{this.$root.published_cost_calculator["Version"]}} was published </span><span> by {{this.$root.published_cost_calculator["Issued by user"]}} at </span><span class="fst-italic"> {{published_at}}</span>
+              </p>
+              <p> No draft cost calculator exists. </p>
+              <p><button class="btn btn-success" @click="create_new_draft"><i class="fas fa-user-lock"></i> Create new draft</button></p>
+            </template>
           </div>
-        </template>
-          <div class="row">
-            <div class="col-3">
-                  <div class="modal fade" id="publish_draft_modal" tabindex="-1" role="dialog" aria-labelledby="publishDraftModalHeader">
-                    <div class="modal-dialog" role="document">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h4 class="modal-title" id="publishDraftModalHeader">Publish new cost calculator</h4>
-                          <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                          <template v-if="this.$root.no_validation_messages">
-                            <p>Are you sure you want to publish the current draft?</p>
-                            <p>This will then become the default cost calculator used for all quotes.</p>
-                            <v-draft-changes-list :modal="true"/>
-                          </template>
-                          <template v-else>
-                            <p>The current draft contains validation errors, please fix these before publishing:</p>
-                            <v-draft-validation-msgs-list :modal="true"/>
-                          </template>
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-                          <template v-if="this.$root.no_validation_messages">
-                            <button id='datepicker-btn' type="button" class="btn btn-primary" @click="publish_draft" data-dismiss="modal">Publish New Cost Calculator</button>
-                          </template>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
+          <template v-if="draft_exists">
+            <div class="row">
+              <div class="col-12">
+                <v-draft-changes-list :modal="false"/>
               </div>
             </div>
+          </template>
+          <div class="modal fade" id="publish_draft_modal" tabindex="-1" role="dialog" aria-labelledby="publishDraftModalHeader">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h4 class="modal-title" id="publishDraftModalHeader">Publish new cost calculator</h4>
+                  <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <template v-if="this.$root.no_validation_messages">
+                    <p>Are you sure you want to publish the current draft?</p>
+                    <p>This will then become the default cost calculator used for all quotes.</p>
+                    <v-draft-changes-list :modal="true"/>
+                  </template>
+                  <template v-else>
+                    <p>The current draft contains validation errors, please fix these before publishing:</p>
+                    <v-draft-validation-msgs-list :modal="true"/>
+                  </template>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                  <template v-if="this.$root.no_validation_messages">
+                    <button id='datepicker-btn' type="button" class="btn btn-primary" @click="publish_draft" data-dismiss="modal">Publish New Cost Calculator</button>
+                  </template>
+                </div>
+              </div>
+            </div>
+          </div>
           <div id="alerts_go_here">
           </div>
           <v-draft-validation-msgs-list :modal="false"/>
@@ -174,7 +171,7 @@ app.component('pricing-preview', {
               <v-products-table :show_discontinued="this.$root.show_discontinued" :quotable="false"/>
             </div>
           </template>
-          </template>
+        </template>
         `
 })
 
