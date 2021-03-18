58ac3361fe9542b07a6a7d4b786494b22f28bf92
sofia.haglund <sofia.haglund@scilifelab.se>
2021-03-18 14:11:23 +0100

    updated pricing links, removed dm from function

--- run_dir/static/js/data_del_plot.js
+++ run_dir/static/js/data_del_plot.js
@@ -20,10 +20,9 @@ function make_plot(key, name, view_type, filter_inst_type){
   function formatBytes(bytes){
       if (bytes === 0) return '0 Bytes';
       const k = 1024;
-      const dm = 2 < 0 ? 0 : 2;
       const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
       const i = Math.floor(Math.log(bytes) / Math.log(k));
-      return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+      return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
   }
   function sumFilesize(series){
       sum = 0
