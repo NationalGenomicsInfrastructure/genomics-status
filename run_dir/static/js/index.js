ce18f9ca402405a9835c17ad6b96931951c7fa2f
sofia.haglund <sofia.haglund@scilifelab.se>
2022-01-31 16:26:21 +0100

    put loader back

--- run_dir/static/js/index.js
+++ run_dir/static/js/index.js
@@ -127,8 +127,6 @@ function plot_sum_data(){
       var sensname = sensordata.sensor_name;
       for (i in timedata) {
         timedata[i][0] = Date.parse(timedata[i][0]);
-        date = new Date()
-        date.setTime(timedata[i][0])
       }
       if (sensname.startsWith('K') || sensname == 'Test F36'){
         var dp_frig = {
@@ -210,8 +208,8 @@ function plot_sum_data(){
       series: freez_series
     });
  });
- // Add a loading spinner and delay to match the loading
- $('#loading_spinner').delay(2500).hide(function(){
+ // Add a loading spinner
+ $('#loading_spinner').delay(1000).hide(function(){
    $('#fridge_sum_plot').show();
    $('#freezer_sum_plot').show();
  });
