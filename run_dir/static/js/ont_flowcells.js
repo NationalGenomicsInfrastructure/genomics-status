d5900c2d0575561a7591c49f44aa4463029690fa
kedhammar <alfred.kedhammar@scilifelab.se>
2022-12-05 15:41:15 +0100

    Add comments and update search bar

--- run_dir/static/js/ont_flowcells.js
+++ run_dir/static/js/ont_flowcells.js
@@ -1,16 +1,31 @@
 
 
 
-
+// When a tab of the tabbable element is clicked, run function() defined here-as
 $(".tabbable").on("click", '[role="tab"]', function() {
 
+    // If the tab being clicked is the ONT tab
     if($(this).attr('href')=='#tab_ont'){
+
+      // Create the table body, initiated with a row equipped with loading spinners
       $("#ont_fc_table_body").html('<tr><td colspan="12" class="text-muted"><span class="fa fa-sync fa-spin"></span> <em>Loading..</em></td></tr>');
+      
+      // Use the API linking to the Python script to fetch data from CouchDB and send the data into function() definied here-as
       return $.getJSON('/api/v1/ont_flowcells', function(data) {
+        
+        // Clear the table body, if any
         $("#ont_fc_table_body").empty();
+        
+        // For every run entry in the fetched data
         $.each(data, function(key, value) {
+          
+          // If there is data in the row
           if(!($.isEmptyObject(value))){
+            
+            // Initiate a new table row
             var tbl_row = $('<tr>');
+            // Append key and subsequent values from the data as html
+
             // Identifiers
             tbl_row.append($('<td>').html(key));
             tbl_row.append($('<td>').html(value['start_date']));
@@ -30,6 +45,8 @@ $(".tabbable").on("click", '[role="tab"]', function() {
             tbl_row.append($('<td>').html(value['basecalled_pass_read_count_format']));
             tbl_row.append($('<td>').html(value['n50_format']));
             tbl_row.append($('<td>').html(value['accuracy']));
+
+          // Append the table row to the table body
           $("#ont_fc_table_body").append(tbl_row);
         }
       });
@@ -42,13 +59,15 @@ $(".tabbable").on("click", '[role="tab"]', function() {
     // Setup - add a text input to each footer cell
     $('#ont_fc_table tfoot th').each( function () {
       var title = $('#ont_fc_table thead th').eq( $(this).index() ).text();
-      $(this).html( '<input size=10 type="text" placeholder="Search..." />' );
+      $(this).html( '<input size=10 type="text" placeholder="Search '+title+'" />' );
     } );
 
+    // Format and sort the finished table
     var table = $('#ont_fc_table').DataTable({
       "paging":false,
       "destroy": true,
       "info":false,
+      // Sort by the (n+1)th column, descending
       "order": [[ 1, "desc" ]]
     });
 
@@ -62,6 +81,7 @@ $(".tabbable").on("click", '[role="tab"]', function() {
     $('#ont_fc_table_filter label input').appendTo($('#ont_fc_table_filter'));
     $('#ont_fc_table_filter label').remove();
     $("#ont_fc_table_filter input").attr("placeholder", "Search...");
+
     // Apply the search
     table.columns().every( function () {
         var that = this;
