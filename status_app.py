6333925f23612360630097de0a26aa1f30436bbc
Johannes Alneberg <alneberg@kth.se>
2023-05-25 14:21:48 +0200

    Specific URL for ONT flowcells

--- status_app.py
+++ status_app.py
@@ -208,8 +208,8 @@ class Application(tornado.web.Application):
             ("/bioinfo/(P[^/]*)$", BioinfoAnalysisHandler),
             ("/deliveries", DeliveriesPageHandler),
             ("/flowcells", FlowcellsHandler),
-            ("/flowcells/(\d{6}_[^/]*)$", FlowcellHandler),         # Illumina run names start w. 6 digits
-            ("/flowcells/(\d{8}_[^/]*)$", ONTFlowcellHandler),      # ONT run names start w. 8
+            ("/flowcells/(\d{6,8}_[^/]*)$", FlowcellHandler),
+            ("/flowcells_ont/(\d{8}_[^/]*)$", ONTFlowcellHandler),
             ("/flowcells/(\d{8}_[^/]*)/[^/]*$", ONTReportHandler),
             ("/flowcells_plot", FlowcellPlotHandler),
             ("/data_delivered_plot", DeliveryPlotHandler),
