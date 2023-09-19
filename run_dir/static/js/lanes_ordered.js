98f35178ed319bc1687b1450794b6392faef3702
Johannes Alneberg <alneberg@kth.se>
2023-09-19 14:49:18 +0200

    Can display a basic tree recursively

--- run_dir/static/js/lanes_ordered.js
+++ run_dir/static/js/lanes_ordered.js
@@ -4,6 +4,94 @@ const vLanesOrderedMain = ({
             fetched_keys: {},
             statistics_data: {}
         }
+    },
+    methods: {
+        fetchStatistics(key1, key2, key3) {
+            url = '/api/v1/lanes_ordered'
+            /* Iterate over the key arguments and abort at first null occurence */
+            var my_arguments = [key1, key2, key3];
+            for (var i = 0; i < my_arguments.length; i++) {
+                var my_key = my_arguments[i];
+                var key_nr = i + 1;
+                // Break if my_key is undefined
+                if (my_key === undefined) {
+                    var nr_of_keys = key_nr - 1;
+                    break;
+                } else {
+                    if (key_nr === 1) {
+                        url += '?key' + key_nr + '=' + my_key
+                    } else {
+                        url += '&key' + key_nr + '=' + my_key
+                    }
+                }
+            }
+
+            axios
+                .get(url)
+                .then(response => {
+                    data = response.data
+                    if (data !== null) {
+                        switch (nr_of_keys) {
+                            case 0: {
+                                this.statistics_data = data
+                                break;
+                            }
+                            case 1: {
+                                this.statistics_data[key1] = {
+                                    ...this.statistics_data[key1],
+                                    ...data
+                                }
+                                break;
+                            }
+                        }
+                    }
+                })
+                .catch(error => {
+                    this.error_messages.push('Unable to fetch statistics, please try again or contact a system administrator.')
+                })
+        },
+        fetchStatisticsDummy() {
+            this.fetchStatistics();
+            /* this.fetchStatistics('aborted'); */
+            this.fetchStatistics('pending');
+        },
     }
 })
-const app = Vue.createApp(vPricingMain)
+
+const app = Vue.createApp(vLanesOrderedMain)
+
+app.component('v-lanes-ordered', {
+    /* Lanes Ordered */
+    mounted: function() {
+        this.$root.fetchStatisticsDummy();
+    },
+    template:
+        /* Using the html comment tag can enable html syntax highlighting in editors */
+        /*html*/`
+        <h4>Lanes Ordered inside Vue</h4>
+        <v-lanes-ordered-item :key_array="[]" :current_key="" :key_level="1" :local_data="this.$root.statistics_data"> </v-lanes-ordered-item>
+        `
+});
+
+
+app.component('v-lanes-ordered-item', {
+    props: ['key_array', 'current_key', 'key_level', 'local_data'],
+    computed: {
+        /* local data keys without the key 'value' */
+        local_data_keys() {
+            return Object.keys(this.local_data).filter(key => key !== 'value')
+        }
+    },
+    template:
+        /*html*/`
+        <li class="ml-3" v-for="category in this.local_data_keys" :key="category">
+            {{category}}: {{this.local_data[category]['value']}} <br/>
+            <template v-if="(Object.keys(this.local_data[category]).length > 1) && (key_level < 3)">
+                <v-lanes-ordered-item :key_array="this.key_array.concat(category)" :current_key="category" :key_level="key_level++" :local_data="local_data[category]"></v-lanes-ordered-item>
+            </template>
+        </li>
+
+        `
+})
+
+app.mount('#lanes_ordered_main');
--- status/lanes_ordered.py
+++ status/lanes_ordered.py
@@ -17,5 +17,21 @@ class LanesOrderedDataHandler(SafeHandler):
     """Delivers data for the Lanes Ordered statistics page"""
 
     def get(self):
-        view = self.application.projects_db.view("project/status_lanes_ordered", group_level=3, reduce=True)
-        self.write(json.dumps(view.rows))
+        key1 = self.get_argument('key1', None)
+        key2 = self.get_argument('key2', None)
+        key3 = self.get_argument('key3', None)
+
+        key_begin = [k for k in [key1,key2,key3] if k is not None]
+        group_level = len(key_begin) + 1 
+        #import pdb; pdb.set_trace()
+
+        if key_begin:
+            key_end = key_begin.append('ZZZ')
+            view = self.application.projects_db.view("project/status_lanes_ordered", group_level=group_level, reduce=True)[key_begin:key_end]
+        else:
+            view = self.application.projects_db.view("project/status_lanes_ordered", group_level=group_level, reduce=True)
+
+        data = {item['key'][group_level-1]: {'value': "{:0.2f}".format(item['value'])} for item in view.rows}
+
+        self.set_header("Content-type", "application/json")
+        self.write(json.dumps(data))
