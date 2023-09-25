970637111fd1b10deb86cf6b57aacdfe9efaaa8a
sofia.haglund <sofia.haglund@scilifelab.se>
2023-09-25 10:15:09 +0200

    removed clipboard script from worksets

--- run_dir/static/js/workset_queues.js
+++ run_dir/static/js/workset_queues.js
@@ -176,15 +176,6 @@ function getDaysAndDateLabel(date, option){
   return [number_of_days, label];
 }
 
-// Copy project samples table to clipboard
-var clipboard = new Clipboard('#samples_copy_table');
-clipboard.on('success', function(e) {
-  e.clearSelection();
-  $('#samples_copy_table_btn').addClass('active').html('<span class="fa fa-copy"></span> Copied!');
-  setTimeout(function(){
-    $('#samples_copy_table_btn').removeClass('active').html('<span class="fa fa-copy"></span> Copy table');
-  }, 2000);
-});
 
 $('.dt-buttons > .buttons-copy').on('click', function () {
   $('.expand-all').click();
