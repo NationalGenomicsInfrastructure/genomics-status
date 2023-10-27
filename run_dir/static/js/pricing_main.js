d4766ebc48796776faf5bda53dece65a57837d7e
sofia.haglund <sofia.haglund@scilifelab.se>
2023-10-27 11:01:04 +0200

    remove space

--- run_dir/static/js/pricing_main.js
+++ run_dir/static/js/pricing_main.js
@@ -803,7 +803,7 @@ app.component('v-product-table-row', {
     },
     methods: {
         add_to_quote(event) {
-            event.preventDefault(); 
+            event.preventDefault();
             if (!(this.product_id in this.$root.quote_prod_ids)) {
                 this.$root.quote_prod_ids[this.product_id] = 0
             }
