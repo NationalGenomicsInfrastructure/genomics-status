662450399809f84a03a635f8a6d4579028287738
sofia.haglund <sofia.haglund@scilifelab.se>
2023-11-20 19:15:19 +0100

    remove auto formatting

--- run_dir/static/js/flowcell.js
+++ run_dir/static/js/flowcell.js
@@ -1,25 +1,25 @@
 /* flowcell.html */
 
-const displayUndetermined = (lane) => {
+const display_undetermined = (lane) => {
     $(`#table_ud_lane_${lane}:first`).slideToggle();
 };
 
 // Copy flowcell lane table to clipboard
 $('.lane-copy').click(() => {
-    const laneBtn = `#${$(this).attr('id')}`;
-    const clipboard = new Clipboard(laneBtn);
+    const lane_btn = `#${$(this).attr('id')}`;
+    const clipboard = new Clipboard(lane_btn);
     clipboard.on('success', (e) => {
         e.clearSelection();
-        $(laneBtn).addClass('active').html('<span class="fa fa-copy"></span> Copied!');
+        $(lane_btn).addClass('active').html('<span class="fa fa-copy"></span> Copied!');
         setTimeout(() => {
-            $(laneBtn).removeClass('active').html('<span class="fa fa-copy"></span> Copy table');
+            $(lane_btn).removeClass('active').html('<span class="fa fa-copy"></span> Copy table');
         }, 2000);
     });
 });
 
 // from runnin_notes.js
 // these vars are needed for load_running_notes(); -> it's js
-const flowcellIdReference = $('#flowcells-js').attr('data-flowcell'); // passing parameters to <script> from flowcell.html
+const flowcell_id_reference = $('#flowcells-js').attr('data-flowcell'); // passing parameters to <script> from flowcell.html
 load_running_notes();
 // from links.js
 load_links();
