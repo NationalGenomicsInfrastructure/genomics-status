923151ceac87a83f561e2401dafe5e04cf61083a
sofia.haglund <sofia.haglund@scilifelab.se>
2023-12-15 15:05:34 +0100

    first draft add ont to fc trends plot

--- status/reads_plot.py
+++ status/reads_plot.py
@@ -17,14 +17,41 @@ class DataFlowcellYieldHandler(SafeHandler):
         else:
             first_term, second_term = search_string.split('-')
 
+        ont_list = self.fetch_ont_data()
+
         docs = [x.value for x in self.application.x_flowcells_db.view("plot/reads_yield")[first_term:second_term+'ZZZZ'].rows]
 
         for doc in docs:
             fc_yield = int(doc.get('total_yield')) / 1000000
             doc['total_yield'] = fc_yield
 
+        docs.extend(ont_list)
+
         self.set_header("Content-type", "application/json")
         self.write(json.dumps(docs))
+
+    def fetch_ont_data(self):
+        ont_list = []
+        docs = [x for x in self.application.nanopore_runs_db.view("info/all_stats")]
+
+        for doc in docs:
+            fc_id = doc.get('key')
+            value_dict = doc.get('value', {})
+            instrument = value_dict.get('instrument')
+            if instrument is None:
+                continue
+            yield_passed = int(value_dict.get('basecalled_pass_bases', 0))
+            read_count_passed = int(value_dict.get('basecalled_pass_read_count', 0))
+            if None in [yield_passed, read_count_passed]:
+                continue
+            if instrument == "PromethION 24":
+                cver = '24'
+            elif instrument == "MN19414":
+                cver = '19414'
+            ont_data = {'id': fc_id, 'instrument': instrument, 'cver': cver, 'yield_passed': yield_passed, 'read_count_passed': read_count_passed}
+            ont_list.append(ont_data)
+
+        return ont_list
 
 
 class FlowcellPlotHandler(SafeHandler):
