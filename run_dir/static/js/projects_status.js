22dc4a3a6b49c36a408747052c36a37933fd3293
Johannes Alneberg <alneberg@kth.se>
2024-03-08 11:15:33 +0100

    Able to filter away all

--- run_dir/static/js/projects_status.js
+++ run_dir/static/js/projects_status.js
@@ -1,18 +1,20 @@
 const vProjectsStatus = {
     data() {
         return {
-            projects: [],
+            all_projects: {},
             sortBy: 'most_recent_date',
             descending: true,
+            status_filter: ['All'],
         }
     },
     computed: {
-        orderedFilteredProjects() {
-            if (this.projects.length == 0) {
-                return this.projects
+        visibleProjects() {
+            /* Filters and sorts the projects */
+            if (Object.keys(this.all_projects).length == 0) {
+                return this.all_projects
             }
 
-            let tempProjects = this.projects
+            let tempProjects = Object.entries(this.all_projects)
 
             // Process search input
             if (this.searchValue != '' && this.searchValue) {
@@ -23,50 +25,71 @@ const vProjectsStatus = {
                 })
             }
 
-            // Sort by most recent date
-            tempProjects = Object.fromEntries(
-                Object.entries(tempProjects).sort((a, b) => {
-                    let proj_a = this.projects[a[0]]
-                    let proj_b = this.projects[b[0]]
-
-                    if (this.sortBy == 'most_recent_date') {
-                        /* First deal with missing dates */
-                        if ((this.mostRecentDate(proj_a) == undefined) && (this.mostRecentDate(proj_b) == undefined)) {
-                            return 0
-                        }
-                        if (this.mostRecentDate(proj_a) == undefined) {
-                            // Missing dates will be the most recent
-                            return 1
-                        } else if (this.mostRecentDate(proj_b) == undefined){
-                            return -1
-                        }
-                        /* Then deal with actual dates */
-                        if (this.mostRecentDate(proj_a) > this.mostRecentDate(proj_b)) {
-                            return 1
-                        } else if (this.mostRecentDate(proj_a) < this.mostRecentDate(proj_b)) {
-                            return -1
-                        }
-                        return 0
-                    };
+            // Process status filter
+            if (!(this.status_filter.includes('All'))) {
+                tempProjects = tempProjects.filter(([project_id, project]) => {
+                    return this.status_filter.includes(project['status_fields']['status'])
                 })
-            )
-
-            if (this.descending == true) {
-                tempProjects = Object.fromEntries(Object.entries(tempProjects).reverse())
             }
 
-            return tempProjects
+            // Sort by most recent date
+            tempProjects = tempProjects.sort((a, b) => {
+                let proj_a = this.all_projects[a[0]]
+                let proj_b = this.all_projects[b[0]]
+
+                if (this.sortBy == 'most_recent_date') {
+                    /* First deal with missing dates */
+                    if ((this.mostRecentDate(proj_a) == undefined) && (this.mostRecentDate(proj_b) == undefined)) {
+                        return 0
+                    }
+                    if (this.mostRecentDate(proj_a) == undefined) {
+                        // Missing dates will be the most recent
+                        return 1
+                    } else if (this.mostRecentDate(proj_b) == undefined){
+                        return -1
+                    }
+                    /* Then deal with actual dates */
+                    if (this.mostRecentDate(proj_a) > this.mostRecentDate(proj_b)) {
+                        return 1
+                    } else if (this.mostRecentDate(proj_a) < this.mostRecentDate(proj_b)) {
+                        return -1
+                    }
+                    return 0
+                };
+            })
+
+            if (this.descending == true) {
+                tempProjects = tempProjects.reverse()
+            }
+
+            return Object.fromEntries(tempProjects)
+        },
+        allStatuses() {
+            let statuses = []
+            for (let project in this.all_projects) {
+                statuses.push(this.all_projects[project]['status_fields']['status'])
+            }
+            // Count the number of each status
+            let statusCounts = {}
+            for (let status of statuses) {
+                if (status in statusCounts) {
+                    statusCounts[status] += 1
+                } else {
+                    statusCounts[status] = 1
+                }
+            }
+            return statusCounts
         }
     },
     methods: {
         // Fetch methods
         fetchProjects() {
             axios
-                .get('/api/v1/projects?list=ongoing&type=All')
+                .get('/api/v1/projects?list=pending,reception_control,review,ongoing&type=All')
                 .then(response => {
                     data = response.data
                     if (data !== null) {
-                        this.projects = data
+                        this.all_projects = data
                     }
                 })
                 .catch(error => {
@@ -124,7 +147,12 @@ app.component('v-projects-status', {
                 Sort ascending
             </template>
         </label>
-        <template v-for="(project, project_id) in this.$root.orderedFilteredProjects" :key="project">
+        <p> All statuses: {{ this.$root.allStatuses }}</p>
+
+        <label>All</label>
+        <input type="checkbox" value="All" v-model="this.$root.status_filter"/>
+
+        <template v-for="(project, project_id) in this.$root.visibleProjects" :key="project">
             <div class="card my-5">
                 <div class="card-header">
                     <div class="d-flex justify-content-between">
@@ -140,11 +168,12 @@ app.component('v-projects-status', {
                     <div class="row">
                         <p> Dates: {{project['summary_dates']}}</p>
                         <p> Most recent date: {{this.$root.mostRecentDateArray(project)[0]}} : {{this.$root.mostRecentDateArray(project)[1]}}</p>
-                        <p> Status: {{ project['status'] }}</p>
+                        <p> Status: {{ project['status_fields']['status'] }}</p>
                         <p> Sequencing Platform: {{ project['sequencing_platform'] }}</p>
                         <p> Flowcell: {{ project['flowcell'] }}</p>
                         <p> Sequencing Setup: {{ project['sequencing_setup'] }}</p>
                         <p> Project Coordinator: {{ project['project_coordinator'] }}</p>
+                        <p> Project all: {{ project }}</p>
                     </div>
                 </div>
             </div>
