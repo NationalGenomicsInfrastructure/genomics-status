2d3a3d0d2f81eb38b5dada13abb0e880d69cb955
chuan-wang <chuan.wang@scilifelab.se>
2024-03-15 08:19:30 +0100

    Add preliminary lane yield threshold for NovaSeqXPlus 1.5B

--- status/flowcells.py
+++ status/flowcells.py
@@ -33,7 +33,8 @@ thresholds = {
     "NovaSeq S1": 650,
     "NovaSeq S2": 1650,
     "NovaSeq S4": 2000,
-    "NovaSeqXPlus 10B": 1000,  # Might need to be reviewed when we settle for a number in AM
+    "NovaSeqXPlus 10B": 1000,
+    "NovaSeqXPlus 1.5B": 600,  # Might need to be reviewed when we settle for a number in AM
     "NextSeq Mid": 25,
     "NextSeq High": 75,
     "NextSeq 2000 P1": 100,
