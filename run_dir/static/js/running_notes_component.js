af4a4af4d34737ce2f137ce46f38ad3b1c1657e2
Johannes Alneberg <alneberg@kth.se>
2024-05-21 17:12:36 +0200

    Filtering and searching project running notes work

--- run_dir/static/js/running_notes_component.js
+++ run_dir/static/js/running_notes_component.js
@@ -2,7 +2,31 @@ export const vRunningNotesTab = {
     props: ['user', 'partition_id'],
     data() {
         return {
-            running_notes: null,
+            running_notes: [],
+            category_filter: 'All',
+            search_term: ''
+        }
+    },
+    computed: {
+        visible_running_notes() {
+            let running_notes_tmp = Object.entries(this.running_notes)
+
+            if (this.search_term !== '') {
+                running_notes_tmp = running_notes_tmp.filter(([running_note_key, running_note]) => {
+                    return (running_note.note.toLowerCase().includes(this.search_term.toLowerCase())) ||
+                                (running_note.user.toLowerCase().includes(this.search_term.toLowerCase())) ||
+                                (running_note.categories.join(' ').toLowerCase().includes(this.search_term.toLowerCase()))
+                })
+            }
+
+            // Filter by category
+            if (this.category_filter !== 'All') {
+                running_notes_tmp = running_notes_tmp.filter(([running_note_key, running_note]) => {
+                    return running_note.categories.includes(this.category_filter)
+                })
+            }
+
+            return Object.fromEntries(running_notes_tmp)
         }
     },
     methods: {
@@ -33,6 +57,9 @@ export const vRunningNotesTab = {
                 new_note_caret.classList.remove('fa-caret-right');
                 new_note_caret.classList.add('fa-caret-down');
             }
+        },
+        setFilter(filter) {
+            this.category_filter = filter
         }
     },
     mounted() {
@@ -41,8 +68,8 @@ export const vRunningNotesTab = {
     template: /*html*/`
     <form action="" method="POST" id="running_notes_form" role="form">
         <div class="card text-dark info-border mb-3 mt-3">
-        <div class="card-header info-bg" @click="toggleNewNoteForm()"><h5><i ref="new_note_caret" class="fas fa-caret-right fa-lg pr-2"></i>Add New Running Note</h5></div>
-        <div ref="new_note_form" class="card-body collapse">
+            <div class="card-header info-bg pt-3" @click="toggleNewNoteForm()"><h5><i ref="new_note_caret" class="fas fa-caret-right fa-lg pr-2"></i>Add New Running Note</h5></div>
+            <div ref="new_note_form" class="card-body collapse">
                 <div class="row">
                     <div class="col form-inline">
                       <label>Choose category:</label>
@@ -88,34 +115,36 @@ export const vRunningNotesTab = {
     <div id="running_notes_filter" class="row" style="margin-bottom:12px;">
       <div class="col-3 ml-2">
         <label>Search :</label>
-        <input type="text" class="form-control" id="rn_search" />
+        <input type="text" v-model="search_term" class="form-control" ref="note_search"/>
       </div>
       <div class="col-2">
         <label>Filter :</label>
+
         <div class="dropdown">
             <button class="btn btn-outline-secondary text-dark dropdown-toggle btn_count" type="button" id="rn_category" data-toggle="dropdown" aria-expanded="false"></button>
             <ul class="dropdown-menu" aria-labelledby="rn_category">
-                <li><button class="dropdown-item" type="button">All</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('All')">All</button></li>
                 <li><div class="dropdown-divider"></div></li>
-                <li><button class="dropdown-item" type="button">Workset</button></li>
-                <li><button class="dropdown-item" type="button">Flowcell</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Workset')">Workset</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Flowcell')">Flowcell</button></li>
+
                 <li><div class="dropdown-divider"></div></li>
-                <li><button class="dropdown-item" type="button">Decision</button></li>
-                <li><button class="dropdown-item" type="button">Lab</button></li>
-                <li><button class="dropdown-item" type="button">Bioinformatics</button></li>
-                <li><button class="dropdown-item" type="button">User Communication</button></li>
-                <li><button class="dropdown-item" type="button">Administration</button></li>
-                <li><button class="dropdown-item" type="button">Important</button></li>
-                <li><button class="dropdown-item" type="button">Invoicing</button></li>
-                <li><button class="dropdown-item" type="button">Deviation</button></li>
-                <li><button class="dropdown-item" type="button">Sticky</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Decision')">Decision</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Lab')">Lab</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Bioinformatics')">Bioinformatics</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('User Communication')">User Communication</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Administration')">Administration</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Important')">Important</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Invoicing')">Invoicing</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Deviation')">Deviation</button></li>
+                <li><button class="dropdown-item" type="button" @click="setFilter('Sticky')">Sticky</button></li>
             </ul>
         </div>
       </div>
     </div>
 
     <!-- display running notes -->
-    <template v-for="running_note in running_notes">
+    <template v-for="running_note in visible_running_notes">
         <v-running-note-single :running_note_obj="running_note" :sticky="false"/>
     </template>
     `
