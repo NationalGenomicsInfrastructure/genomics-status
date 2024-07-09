66088da6c1f007274750bd1c4d9376a8f9e7dbc0
Anandashankar Anil <anandasanil@gmail.com>
2024-07-09 15:50:32 +0200

    Update generated excel file to new pattern sent from kth

--- status/invoicing.py
+++ status/invoicing.py
@@ -213,12 +213,12 @@ class GenerateInvoiceHandler(AgreementsDBHandler, InvoicingDataHandler):
                     "1,00",
                     f'({proj_specs["cust_desc"]})',
                     account_dets["fakturaunderlag"],
-                    contact_dets["email"],
                     account_dets["fakturafragor"],
                     account_dets["support_email"],
                     " ",
+                    account_dets["artikelnr"],
                     " ",
-                    " ",
+                    account_dets["ftg"],
                     account_dets["unit"],
                     account_dets["number"],
                     " ",
@@ -272,11 +272,12 @@ class GenerateInvoiceHandler(AgreementsDBHandler, InvoicingDataHandler):
         account_dets["fakturaunderlag"] = inv_defs["account_details"]["fakturaunderlag"]
         account_dets["fakturafragor"] = inv_defs["account_details"]["fakturafragor"]
         account_dets["support_email"] = inv_defs["account_details"]["support_email"]
+        account_dets["artikelnr"] = inv_defs["account_details"]["artikelnr"]
+        account_dets["ftg"] = inv_defs["account_details"]["ftg"]
 
         contact_dets = {}
         fields = self.get_order_details(proj_doc["order_details"]["identifier"])
         contact_dets["name"] = fields["project_pi_name"]
-        contact_dets["email"] = fields["project_pi_email"]
         contact_dets["reference"] = fields["project_invoice_ref"]
         contact_dets["invoice_address"] = fields["address_invoice_address"]
         contact_dets["invoice_zip"] = fields["address_invoice_zip"]
