3b9f17851999508190cdb6640de4469361f8026d
Johannes Alneberg <alneberg@kth.se>
2024-11-28 15:07:00 +0100

    Removed a console.log statement

--- run_dir/static/js/projects_components.js
+++ run_dir/static/js/projects_components.js
@@ -103,7 +103,6 @@ export const vProjectDetails = {
         copy_to_clipboard(event, text) {
             // Copy text to clipboard
             navigator.clipboard.writeText(text).then(function() {
-                console.log('Async: Copying to clipboard was successful!');
                 // Show a tooltip
                 let tooltip = new bootstrap.Tooltip(event.target, {
                     title: 'Copied!',
