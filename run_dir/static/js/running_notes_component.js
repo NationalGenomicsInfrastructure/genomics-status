04eb581397743bf57f68baa486958ccc72b7199c
Anandashankar Anil <anandasanil@gmail.com>
2024-12-10 14:10:57 +0100

    Make markdown help a card in the same layer instead of a modal

--- run_dir/static/js/running_notes_component.js
+++ run_dir/static/js/running_notes_component.js
@@ -10,6 +10,7 @@ export const vRunningNotesTab = {
             running_notes: [],
             search_term: '',
             submitting: false,
+            show_help: false,
             cat_classes: {
                 'automatic': {
                                 'Workset': ['primary', 'calendar-plus'],
@@ -229,12 +230,9 @@ export const vRunningNotesTab = {
             }
         },
 
-        /* Markdown Help Modal */
-        closeHelpModal() {
-            this.help_modal.hide();
-        },
-        openHelpModal() {
-            this.help_modal.show();
+        /* Toggle Markdown Help visibility*/
+        showMarkdownHelp() {
+            this.show_help = !this.show_help;
         },
 
         count_cards(category) {
@@ -260,7 +258,6 @@ export const vRunningNotesTab = {
             return
         }
         this.fetchAllRunningNotes(this.partition_id);
-        this.help_modal = new bootstrap.Modal(this.$refs.help_modal);
     },
     template: /*html*/`
     <div class="card text-dark info-border mb-3 mt-3">
@@ -307,7 +304,7 @@ export const vRunningNotesTab = {
             </div>
             <div class="row">
                 <div class="col-md-6 text-right">
-                    <button type="button" class="btn btn-link" @click=openHelpModal>Markdown Help</button>
+                    <button type="button" class="btn btn-link" @click=showMarkdownHelp>Markdown Help</button>
                     <template v-if="submitting">
                         <button class="btn btn-primary" type="button" disabled>
                             <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
@@ -318,6 +315,102 @@ export const vRunningNotesTab = {
                        <button type="submit" class="btn btn-primary" id="save_note_button" @click.prevent="submitRunningNote">Submit Running Note</button>
                     </template>
                 </div>
+            </div>
+            <div class="row justify-content-md-center" v-show="show_help">
+              <div class="col-md-9 mt-4">
+                <div class="card">
+                    <div class="card-header" id="markdown_help">
+                        <h3 class="card-title">Running Notes - Markdown Help</h3>
+                    </div>
+                    <div class="card-body">
+                        <p><strong>Remember:</strong> <u>Two</u> line breaks are required to split a line in two!</p>
+                        <table class="table table-bordered">
+                            <tbody>
+                                <tr class="darkth">
+                                    <th>You Write</th>
+                                    <th>Running Note Shows</th>
+                                </tr>
+                                <tr>
+                                    <td>Some *italic* text</td>
+                                    <td class="mkdown">Some <em>italic</em> text</td>
+                                </tr>
+                                <tr>
+                                    <td>Some **bold** text</td>
+                                    <td class="mkdown">Some <strong>bold</strong> text</td>
+                                </tr>
+                                <tr>
+                                    <td>Some ***bold italic*** text</td>
+                                    <td class="mkdown">Some <strong><em>bold italic</em></strong> text</td>
+                                </tr>
+                                <tr>
+                                    <td>* Bullet pointed<br>* List of items<br>&nbsp;* With nested<br>* Items</td>
+                                    <td class="mkdown">
+                                        <ul>
+                                            <li>Bullet pointed</li>
+                                            <li>List of items
+                                                <ul>
+                                                    <li>With nested</li>
+                                                </ul>
+                                            </li>
+                                            <li>Items</li>
+                                        </ul>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>1. Numbered list<br>1. Of items</td>
+                                    <td class="mkdown">
+                                        <ol>
+                                            <li>Numbered list</li>
+                                            <li>Of items</li>
+                                        </ol>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Links [are easy](https://genomics-status.scilifelab.se/)!</td>
+                                    <td class="mkdown">Links <a href="https://genomics-status.scilifelab.se/">are easy</a>!</td>
+                                </tr>
+                                <tr>
+                                    <td>You can do e-mail addresses too: &lt;genomics_support@scilifelab.se&gt;</td>
+                                    <td>You can do e-mail addresses too: <a href="mailto:genomics_support@scilifelab.se">genomics_support@scilifelab.se</a></td>
+                                </tr>
+                                <tr>
+                                    <td>Markdown even has a logo: ![Awesome Logo](https://genomics-status.scilifelab.se/static/img/markdown.png)</td>
+                                    <td class="mkdown">Markdown even has a logo: <img src="/static/img/markdown.png" title="Awesome Logo"></td>
+                                </tr>
+                                <tr>
+                                    <td>You can put things like flow cell IDs in \`back ticks\`</td>
+                                    <td class="mkdown">You can put things like flow cell IDs in <code>back ticks</code></td>
+                                </tr>
+                                <tr>
+                                    <td>\`\`\`<br>Larger chunks of code-like stuff<br>can go in 'code-fences' of<br>three back ticks<br>\`\`\`</td>
+                                    <td class="mkdown">
+                                        <pre><code>Larger chunks for code-like stuff<br>can go in 'code-fences' of<br>three back ticks</code></pre>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>&gt; You can quote someone<br>&gt; with greater than symbols</td>
+                                    <td class="mkdown"><blockquote>You can quote someone with greater than symbols</blockquote></td>
+                                </tr>
+                                <tr>
+                                    <td>Split up content<br>***<br>with three or more asterisks</td>
+                                    <td>Split up content<hr>with three or more asterisks</td>
+                                </tr>
+                                <tr>
+                                    <td># Headings<br>## Use these<br>### Symbols</td>
+                                    <td>
+                                        <h1>Headings</h1>
+                                        <h2>Use these</h2>
+                                        <h3>Symbols</h3>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <p>For further reference, see the <a href="http://daringfireball.net/projects/markdown/syntax" target="_blank">syntax document</a>.
+                            Live editing tools such as <a href="http://dillinger.io/" target="_blank">dillinger.io</a> or
+                            <a href="http://25.io/mou/" target="_blank">Mou</a> may also be of use.</p>
+                    </div>
+                </div>
+               </div>
             </div>
             </fieldset>
         </form>
@@ -356,109 +449,6 @@ export const vRunningNotesTab = {
             </ul>
         </div>
       </div>
-    </div>
-
-
-    <!-- RUNNING NOTES HELP -->
-    <div id="markdown_help" class="modal fade" ref="help_modal">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h3 class="modal-title">Running Notes - Markdown Help</h3>
-                    <button type="button" class="btn-close" @click="closeHelpModal" aria-hidden="true"></button>
-                </div>
-                <div class="modal-body">
-                    <p><strong>Remember:</strong> <u>Two</u> line breaks are required to split a line in two!</p>
-                    <table class="table table-bordered">
-                        <tbody>
-                            <tr class="darkth">
-                                <th>You Write</th>
-                                <th>Running Note Shows</th>
-                            </tr>
-                            <tr>
-                                <td>Some *italic* text</td>
-                                <td class="mkdown">Some <em>italic</em> text</td>
-                            </tr>
-                            <tr>
-                                <td>Some **bold** text</td>
-                                <td class="mkdown">Some <strong>bold</strong> text</td>
-                            </tr>
-                            <tr>
-                                <td>Some ***bold italic*** text</td>
-                                <td class="mkdown">Some <strong><em>bold italic</em></strong> text</td>
-                            </tr>
-                            <tr>
-                                <td>* Bullet pointed<br>* List of items<br>&nbsp;* With nested<br>* Items</td>
-                                <td class="mkdown">
-                                    <ul>
-                                        <li>Bullet pointed</li>
-                                        <li>List of items
-                                            <ul>
-                                                <li>With nested</li>
-                                            </ul>
-                                        </li>
-                                        <li>Items</li>
-                                    </ul>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>1. Numbered list<br>1. Of items</td>
-                                <td class="mkdown">
-                                    <ol>
-                                        <li>Numbered list</li>
-                                        <li>Of items</li>
-                                    </ol>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Links [are easy](https://genomics-status.scilifelab.se/)!</td>
-                                <td class="mkdown">Links <a href="https://genomics-status.scilifelab.se/">are easy</a>!</td>
-                            </tr>
-                            <tr>
-                                <td>You can do e-mail addresses too: &lt;genomics_support@scilifelab.se&gt;</td>
-                                <td>You can do e-mail addresses too: <a href="mailto:genomics_support@scilifelab.se">genomics_support@scilifelab.se</a></td>
-                            </tr>
-                            <tr>
-                                <td>Markdown even has a logo: ![Awesome Logo](https://genomics-status.scilifelab.se/static/img/markdown.png)</td>
-                                <td class="mkdown">Markdown even has a logo: <img src="/static/img/markdown.png" title="Awesome Logo"></td>
-                            </tr>
-                            <tr>
-                                <td>You can put things like flow cell IDs in \`back ticks\`</td>
-                                <td class="mkdown">You can put things like flow cell IDs in <code>back ticks</code></td>
-                            </tr>
-                            <tr>
-                                <td>\`\`\`<br>Larger chunks of code-like stuff<br>can go in 'code-fences' of<br>three back ticks<br>\`\`\`</td>
-                                <td class="mkdown">
-                                    <pre><code>Larger chunks for code-like stuff<br>can go in 'code-fences' of<br>three back ticks</code></pre>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>&gt; You can quote someone<br>&gt; with greater than symbols</td>
-                                <td class="mkdown"><blockquote>You can quote someone with greater than symbols</blockquote></td>
-                            </tr>
-                            <tr>
-                                <td>Split up content<br>***<br>with three or more asterisks</td>
-                                <td>Split up content<hr>with three or more asterisks</td>
-                            </tr>
-                            <tr>
-                                <td># Headings<br>## Use these<br>### Symbols</td>
-                                <td>
-                                    <h1>Headings</h1>
-                                    <h2>Use these</h2>
-                                    <h3>Symbols</h3>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <p>For further reference, see the <a href="http://daringfireball.net/projects/markdown/syntax" target="_blank">syntax document</a>.
-                        Live editing tools such as <a href="http://dillinger.io/" target="_blank">dillinger.io</a> or
-                        <a href="http://25.io/mou/" target="_blank">Mou</a> may also be of use.</p>
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-primary" @click="closeHelpModal" aria-hidden="true">Close</button>
-                </div>
-            </div>
-        </div>
     </div>
 
     <!-- display running notes -->
