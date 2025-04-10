85675bd77f898dda5bec6c8ab841ab6360522f5a
Johannes Alneberg <alneberg@kth.se>
2025-04-10 14:59:59 +0200

    Stashing project creation changes

--- run_dir/static/js/project_creation.js
+++ run_dir/static/js/project_creation.js
@@ -1,13 +1,22 @@
 const vProjectCreationForm = {
     data() {
         return {
-            json_form: {},
-            error_messages: []
+          json_form: {},
+          error_messages: []
         }
     },
     computed: {
         description() {
             return this.json_form.description
+        },
+        instruction() {
+            return this.json_form.instruction
+        },
+        fields() {
+            return this.json_form.fields
+        },
+        title() {
+            return this.json_form.title
         }
     },
     methods: {
@@ -19,7 +28,7 @@ const vProjectCreationForm = {
             axios
                 .get(url)
                 .then(response => {
-                    this.json_form = response.data
+                    this.json_form = response.data.form
                 })
                 .catch(error => {
                     this.error_messages.push('Error fetching form. Please try again or contact a system adminstrator.');
@@ -35,13 +44,38 @@ const vProjectCreationForm = {
         <div class="container">
             <div class="row">
                 <div class="col-12">
+                    <h1>Json Forms:</h1>
+                    <json-forms
+                      :data="data"
+                      :schema="schema"
+                      :uischema="uischema"
+                      :renderers="renderers"
+                      @change="onChange"
+                    />
                     <template v-if="error_messages.length > 0">
                         <div class="alert alert-danger" role="alert">
                             <h3 v-for="error in error_messages">{{ error }}</h3>
                         </div>
                     </template>
-                    <h1>Project Creation Form</h1>
-                    <p>Form for creating a project</p>
+                    <h1>{{ title }}</h1>
+                    <h2>{{ description }}</h2>
+                    <p>{{ instruction }}</p>
+
+                    <div>
+                        <template v-for="field in fields">
+                            <template v-if="field.type === 'group'">
+                                <div>
+                                    <h4>{{ field.label }}</h4>
+                                    <template v-for="subfield in field.fields">
+                                        <v-form-field :field="subfield"></v-form-field>
+                                    </template>
+                                </div>
+                            </template>
+                            <template v-else>
+                                <v-form-field :field="field"></v-form-field>
+                            </template>
+                        </template>
+                    </div>
                     <div>
                         <pre>{{ json_form }}</pre>
                     </div>
@@ -50,6 +84,29 @@ const vProjectCreationForm = {
         </div>`
 }
 
+const vFormField = {
+    name: 'v-form-field',
+    props: ['field'],
+    template: 
+        /*html*/`
+        <div>
+            <template v-if="field.type === 'select'">
+                <label :for="field.identifier" class="form-label">{{ field.label }}</label>
+                <select class="form-select" :aria-label="field.description">
+                    <template v-for="option in field.select_options">
+                        <option :value="option">{{option}}</option>
+                    </template>
+                </select>
+            </template>
+            <template v-else-if="field.type === 'text'">
+                <label :for="field.name">{{ field.label }}</label>
+                <input :type="field.type" :name="field.name" :id="field.name" :placeholder="field.placeholder">
+            </template>
+            <p>{{ field.description }}</p>
+        </div>`
+}
+
 
 const app = Vue.createApp(vProjectCreationForm)
+app.component('v-form-field', vFormField)
 app.mount('#v_project_creation')
--- status/project_creation.py
+++ status/project_creation.py
@@ -11,3 +11,33 @@ class ProjectCreationHandler(SafeHandler):
                 user=self.get_current_user(),
             )
         )
+
+
+class ProjectCreationFormDataHandler(SafeHandler):
+    def get(self):
+        # Fetch latest form from couchdb using cloudant
+
+        all_valid_docs = self.application.cloudant.post_view(
+            db="project_creation_forms",
+            ddoc="by_creation_date",
+            view="valid",
+            limit=1,
+            descending=True,
+            include_docs=True,
+        ).get_result()
+
+        self.set_header("Content-type", "application/json")
+
+        if not all_valid_docs or "rows" not in all_valid_docs:
+            self.set_status(400)
+            return self.write("Error: no valid forms found")
+
+        if len(all_valid_docs["rows"]) == 0:
+            self.set_status(400)
+            return self.write("Error: no valid forms found")
+
+        if "doc" not in all_valid_docs["rows"][0]:
+            self.set_status(400)
+            return self.write("Error: no valid forms found. Doc is missing")
+
+        return self.write({"form": all_valid_docs["rows"][0]["doc"]})
