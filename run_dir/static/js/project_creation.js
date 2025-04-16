a63174bc3c33ad252115517d890050ab187836e2
Johannes Alneberg <alneberg@kth.se>
2025-04-16 17:19:43 +0200

    Started with a new form for updating the project creation form, the validation is not triggered, dont't know why

--- run_dir/static/js/project_creation.js
+++ run_dir/static/js/project_creation.js
@@ -8,6 +8,8 @@ const vProjectCreationForm = {
           error_messages: [],
           formData: {},
           json_form: {},
+          new_json_schema: {},
+          initiated: false,
           showDebug: false,
           validation_errors: [],
           validation_errors_per_field: {}
@@ -74,10 +76,20 @@ const vProjectCreationForm = {
         },
         getValue(object, field) {
             // Get the value of a field or return a default value
+            if (object === undefined) {
+                return undefined
+            }
             if (object[field] !== undefined) {
                 return object[field]
             }
             return undefined
+        },
+        startNewForm(){
+            // Only allow it to be iniatied once.
+            if (!this.initiated) {
+                this.new_json_schema = JSON.parse(JSON.stringify(this.$root.json_form));
+                this.initiated = true;
+            }
         },
         submitForm() {
             axios
@@ -127,7 +139,6 @@ const vProjectCreationForm = {
 
             if (!valid) {
                 // Loop over the errors
-                console.log(validate.errors)
                 validate.errors.forEach(error => {
                     // Check if the instance path is a field
                     if (error.instancePath !== '') {
@@ -141,6 +152,12 @@ const vProjectCreationForm = {
                 return false;
             }
             return true;
+        },
+        validateNew() {
+            // Validate the new JSON schema
+            console.log("Validating new schema");
+            const ajv = new Ajv({strictSchema: false, allErrors: true});
+            const validate = ajv.compile(this.new_json_schema);
         }
     },
     mounted() {
@@ -150,6 +167,10 @@ const vProjectCreationForm = {
         formData: {
             deep: true,
             handler: 'updateOptionsAndValidate'
+        },
+        new_json_schema: {
+            deep: true,
+            handler: 'validateNew()'
         }
     },
     template: 
@@ -203,7 +224,9 @@ const vProjectCreationForm = {
                     </form>
                 </template>
             </div>
-        </div>`
+        </div>
+        <v-create-form></v-create-form>
+        `
 }
 
 const vFormField = {
@@ -324,6 +347,141 @@ const vFormField = {
         </div>`
 }
 
+const vCreateForm = {
+    name: 'v-create-form',
+    data() {
+        return {
+            showDebug: false,
+        }
+    },
+    computed: {
+        newForm() {
+            return this.$root.getValue(this.$root.new_json_schema, 'json_schema');
+        },
+        fields() {
+            return this.$root.getValue(this.newForm, 'properties');
+        },
+    },
+    template: 
+        /*html*/`
+        <div class="container">
+            <div class="row mt-5">
+                <template v-if="new_json_schema === {}">
+                    <div class="alert alert-info" role="alert">
+                        <h3>Loading...</h3>
+                    </div>
+                </template>
+                <template v-else>
+                    <div class="row">
+                        <div class="col-auto">
+                            <h1>Update Form: {{title}}</h1>
+                        </div>
+                        <div class="col-auto ml-auto">
+                            <button class="btn btn-sm btn-secondary" @click="showDebug = !showDebug">
+                                <i class="fa fa-bug mr-2"></i>Toggle Debug Info
+                            </button>
+                        </div>
+                    </div>
+                    <template v-if="showDebug" class="card">
+                        <pre>{{ this.$root.new_json_schema }}</pre>
+                    </template>
+                    <button class="btn btn-lg btn-primary mt-3" @click="this.$root.startNewForm">Start new form</button>
+                    <template v-for="(field, identifier) in fields" :key="identifier">
+                        <template v-if="field.ngi_form_type !== undefined">
+                            <v-update-form-field :field="field" :identifier="identifier"></v-update-form-field>
+                        </template>
+                    </template>
+                </template>
+            </div>
+        </div>
+    `
+}
+
+const vUpdateFormField = {
+    name: 'v-update-form-field',
+    props: ['field', 'identifier'],
+    computed: {        
+        description() {
+            return this.field.description;
+        },
+        form_type() {
+            return this.field.ngi_form_type;
+        },
+        label() {
+            return this.field.ngi_form_label;
+        },
+        new_form() {
+            return this.$root.getValue(this.$root.new_json_schema, 'json_schema');
+        },
+        current_value() {
+            return this.$root.formData[this.identifier];
+        },
+        type() {
+            return this.field.type;
+        }
+    },
+    template:
+        /*html*/`
+        <div class="mb-5">
+            <h2>{{this.label}}</h2>
+            <div>
+                <label :for="identifier" class="form-label">Identifier</label>
+                <input :id="identifier" class="form-control" type="string"  v-model="identifier" disabled>
+            </div>
+            <div>
+                <label :for="identifier + '_description'" class="form-label">Description</label>
+                <input :id="identifier + '_description'" class="form-control" type="string" v-model="this.new_form['properties'][identifier]['description']">
+            </div>
+            <div>
+                <label :for="identifier + '_default'" class="form-label">Default value</label>
+                <input :id="identifier + '_default'" class="form-control" type="string" v-model="this.new_form['properties'][identifier]['default']">
+            </div>
+            <div>
+                <label :for="identifier + '_ngi_form_type'" class="form-label">Form Type</label>
+                <select :id="identifier + '_ngi_form_type'" class="form-control" v-model="this.new_form['properties'][identifier]['ngi_form_type']">
+                    <option value="string">String</option>
+                    <option value="boolean">Boolean</option>
+                    <option value="select">Select</option>
+                    <option value="datalist">Datalist</option>
+                </select>
+
+            </div>
+            <div>
+                <label :for="identifier + '_ngi_form_label'" class="form-label">Label</label>
+                <input :id="identifier + '_ngi_form_label'" class="form-control" type="string" v-model="this.new_form['properties'][identifier]['ngi_form_label']">
+            </div>
+            <div>
+                <label :for="identifier + '_ngi_form_lims_udf'" class="form-label">LIMS UDF</label>
+                <input :id="identifier + '_ngi_form_lims_udf'" class="form-control" type="string" v-model="this.new_form['properties'][identifier]['ngi_form_lims_udf']">
+            </div>
+            <template v-if="(this.type === 'string') && (this.form_type !== 'select')">
+                <div>
+                    <label :for="identifier + '_pattern'" class="form-label">Pattern</label>
+                    <input :id="identifier + '_pattern'" class="form-control" type="string" v-model="this.new_form['properties'][identifier]['pattern']">
+                </div>
+                <div class="form-check form-switch">
+                    <!-- Get Suggestions -->
+                    <label :for="identifier + '_ngi_form_get_suggestions'" class="form-check-label">Get Suggestions (will fetch used values to suggest) </label>
+                    <input :for="identifier + '_ngi_form_get_suggestions'" class="form-check-input" type="checkbox" v-model="this.new_form['properties'][identifier]['ngi_form_get_suggestions']">
+                </div>
+            </template>
+            <template v-if="(this.form_type === 'select') || (this.form_type === 'datalist')">
+                <div class="col-6">
+                    <h3>Allowed values</h3>
+                    <template v-for="(option, index) in this.new_form['properties'][identifier]['enum']" :key="index">
+                        <div class="input-group mb-3">
+                            <input :id="identifier + '_enum_'+index" class="form-control col-auto" type="string" v-model="this.new_form['properties'][identifier]['enum'][index]">
+                            <button class="btn btn-danger col-auto" @click.prevent="this.new_form['properties'][identifier]['enum'].splice(index, 1)">Remove</button>
+                        </div>
+                    </template>
+                    <button class="btn btn-primary" @click.prevent="this.new_form['properties'][identifier]['enum'].push('')">Add new allowed value</button>
+                </div>
+            </template>
+        </div>`
+}
+
 const app = Vue.createApp(vProjectCreationForm)
 app.component('v-form-field', vFormField)
+app.component('v-create-form', vCreateForm)
+app.component('v-update-form-field', vUpdateFormField)
 app.mount('#v_project_creation')
