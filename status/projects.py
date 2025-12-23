470e2a42b5eb8d2b42a197ae02e22af593a9047f
Anandashankar Anil <anandasanil@gmail.com>
2025-12-23 12:35:41 +0100

    Move reads_sequenced calculation to project page

--- status/projects.py
+++ status/projects.py
@@ -259,21 +259,6 @@ class ProjectsBaseDataHandler(SafeHandler):
                 "Email to project bioinformatics responsible, from Order Portal"
             )
 
-        all_reads = ReadsTotalHandler.get_total_reads(self.application, row["key"][1])
-        reads_sum = 0
-        for flowcells in all_reads.values():
-            for flowcell in flowcells:
-                if flowcell["sample_status"] not in ["Failed"]:
-                    reads_sum += flowcell["cl"]
-
-        # 1 unit = 60 million reads
-        row["value"]["reads_sequenced"] = (
-            str(reads_sum) + f" (~ {round(reads_sum / 600000000, 2)} units)"
-        )
-        field_sources["reads_sequenced"] = (
-            "Calculated by Genomics Status (backend) from all non-failed samples in the project"
-        )
-
         return row, field_sources
 
     def _get_two_year_from(self, from_date):
@@ -723,6 +708,23 @@ class ProjectDataHandler(ProjectsBaseDataHandler):
 
         summary_row = view_rows[0]
         summary_row, field_sources = self.project_summary_data(summary_row)
+
+        # Calculate total reads sequenced for non-failed samples in the project
+        all_reads = ReadsTotalHandler.get_total_reads(self.application, project)
+        reads_sum = 0
+        for flowcells in all_reads.values():
+            for flowcell in flowcells:
+                # Include all non-failed samples, even if they don't have a sample_status
+                if flowcell.get("sample_status", {}) not in ["Failed"]:
+                    reads_sum += flowcell["cl"]
+
+        # 1 unit = 600 million reads
+        summary_row["value"]["reads_sequenced"] = (
+            str(reads_sum) + f" (~ {round(reads_sum / 600000000, 2)} units)"
+        )
+        field_sources["reads_sequenced"] = (
+            "Calculated by Genomics Status (backend) from all non-failed samples in the project"
+        )
 
         date_result_rows = self.application.cloudant.post_view(
             db="projects",
