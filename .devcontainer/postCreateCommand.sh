f7c23ced7d7f7a0000be15e4e1a2d977980ddce6
Johannes Alneberg <alneberg@kth.se>
2026-06-29 11:15:19 +0200

    Fixing devcontainer with new conf and docker compose setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,5 +5,3 @@
 echo $PATH
 
 python -m pip install -e .
-ln -s /home/mambauser/conf/.genologicsrc /home/mambauser/.genologicsrc
-ln -s /home/mambauser/conf/genosqlrc.yaml /home/mambauser/.genosqlrc.yaml
